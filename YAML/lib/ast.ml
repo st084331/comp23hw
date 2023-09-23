5dd5805062d0bbf25b8e6c92267ede218024cb4d
Drumov Maxim <Drumov.M.A@gmail.com>
2023-09-23 11:31:21 +0300

    Add deriving show, change binop
    Signed-off-by: Drumov Maxim <Drumov.M.A@gmail.com>

--- YAML/lib/ast.ml
+++ YAML/lib/ast.ml
@@ -1,28 +1,32 @@
 type const =
   | CInt of int
   | CBool of bool
+[@@deriving show { with_path = false }]
 
 type bin_op =
-  | Plus of const * const
-  | Minus of const * const
-  | Slash of const * const
-  | Aster of const * const
-  | Xor of const * const
-  | And of const * const
-  | Or of const * const
-  | Equal of const * const
-  | NotEqual of const * const
-  | Gt of const * const
-  | Lt of const * const
-  | Gte of const * const
-  | Lte of const * const
+  | Add
+  | Sub
+  | Div
+  | Mul
+  | Xor
+  | And
+  | Or
+  | Eq
+  | NEq
+  | Gt
+  | Lt
+  | Gte
+  | Lte
+[@@deriving show { with_path = false }]
 
 type expr =
-  | Const of const
-  | Binop of bin_op
-  | App of expr * expr
-  | Let of string * expr * expr
-  | LetRec of string * expr * expr
-  | LetIn of string * expr * expr
-  | LetRecIn of string * expr * expr
-  | Fun of expr * expr
+  | EConst of const
+  | EVar of string
+  | EBinop of bin_op * expr * expr
+  | EApp of expr * expr
+  | ELet of string * expr * expr
+  | ELetRec of string * expr * expr
+  | ELetIn of string * expr * expr
+  | ELetRecIn of string * expr * expr
+  | EFun of expr * expr
+[@@deriving show { with_path = false }]
