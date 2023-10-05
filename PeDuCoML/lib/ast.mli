76fb1c291ceec6c9367ebf35cf9ace994d19198a
Ilya Dudnikov <dudnikoff98@gmail.com>
2023-10-05 17:30:04 +0300

    Add patterns to AST

--- PeDuCoML/lib/ast.mli
+++ PeDuCoML/lib/ast.mli
@@ -35,13 +35,21 @@ type expression =
   | EUnaryOperation of unary_operator * expression (** -(1 + 3) *)
   | EApplication of expression * expression (** f x *)
   | EIdentifier of id (** x *)
-  | EFun of id list * expression (** fun x y -> x + y *)
+  | EFun of pattern list * expression (** fun x y -> x + y *)
   | EList of expression list (** [ 1; 2; 3 ] *)
   | EConstructList of expression * expression (** 1 :: [2; 3] *)
   | ETuple of expression list (** (1, "Vasya Pupkin", '\n') *)
-  | EDeclaration of id * id list * expression (** let add x y = x + y *)
-  | ERecursiveDeclaration of id * id list * expression
+  | EDeclaration of id * pattern list * expression (** let add x y = x + y *)
+  | ERecursiveDeclaration of id * pattern list * expression
   (** let rec factorial n = n * factorial (n - 1) *)
-  | ELetIn of expression list * expression (** let x = 1 and y = 2 in x + y *)
+  | ELetIn of pattern list * expression (** let x = 1 and y = 2 in x + y *)
   | EIf of expression * expression * expression (** if true then 1 else 0 *)
-  | EMatchWith of expression * (expression * expression) list (** match x with _ -> x *)
+  | EMatchWith of expression * (pattern * expression) list (** match x with _ -> x *)
+
+and pattern =
+  | PLiteral of literal (** true *)
+  | PWild (** _ *)
+  | PTuple of pattern list (** (1, 2) *)
+  | PList of pattern list (** [a; b; c] *)
+  | PConstructList of pattern * pattern (** a :: [b; c] *)
+  | PIdentifier of id (** cool_variable *)
