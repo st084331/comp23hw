7b0be15b79f5f1a85e5517090607ceeb409d43a3
Drumov Maxim <Drumov.M.A@gmail.com>
2023-10-10 13:45:06 +0300

    Add condition expression
    Signed-off-by: Drumov Maxim <Drumov.M.A@gmail.com>

---
+++ YaML/lib/ast.ml
@@ -0,0 +1,33 @@
+type const =
+  | CInt of int
+  | CBool of bool
+[@@deriving show { with_path = false }]
+
+type bin_op =
+  | Add
+  | Sub
+  | Div
+  | Mul
+  | Xor
+  | And
+  | Or
+  | Eq
+  | NEq
+  | Gt
+  | Lt
+  | Gte
+  | Lte
+[@@deriving show { with_path = false }]
+
+type expr =
+  | EConst of const
+  | EVar of string
+  | EBinop of bin_op * expr * expr
+  | EApp of expr * expr
+  | EIfThenElse of expr * expr * expr
+  | ELet of string * expr
+  | ELetRec of string * expr
+  | ELetIn of string * expr * expr
+  | ELetRecIn of string * expr * expr
+  | EFun of expr * expr
+[@@deriving show { with_path = false }]
