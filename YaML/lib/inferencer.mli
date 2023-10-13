e2b3addf357a75a7afd335f9222a27b3975f920a
Ilya Pankratov <i.pankratov.main@gmail.com>
2023-10-13 19:37:08 +0300

    Add .mli file for inferencer
    Signed-off-by: Ilya Pankratov <i.pankratov.main@gmail.com>

---
+++ YaML/lib/inferencer.mli
@@ -0,0 +1,14 @@
+(** Type inferencer errors *)
+
+type error =
+  [ `Occurs_check
+  | `No_variable of string
+  | `Unification_failed of Typetree.ty * Typetree.ty
+  | `TodoError
+  ]
+
+(** Pretty printer for errors *)
+val pp_error : Format.formatter -> error -> unit
+
+(** Infer type of expesstion *)
+val infer_expr : Ast.expr -> (Typetree.ty * Typedtree.texpr, error) result
