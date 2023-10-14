7b73527ad9bb6d1ff3e9ba92ed5c980a950e4425
Anton-Kraev <antonkraeww@gmail.com>
2023-10-14 21:05:56 +0300

    added expression dispatch

--- KREML/lib/parser.ml
+++ KREML/lib/parser.ml
@@ -8,7 +8,6 @@ let is_space = function
 ;;
 
 let skip = take_while is_space
-let spaces = take_while1 is_space
 let trim p = skip *> p <* skip
 let parens p = skip *> char '(' *> skip *> p <* skip <* char ')'
 
@@ -49,15 +48,49 @@ let varname =
 
 (* dispatch *)
 type dispatch =
-  { expression_p : dispatch -> expr t
-  ; declaration_p : dispatch -> decl t
+  { literal_p : expr t
+  ; identifier_p : expr t
+  ; unary_op_p : dispatch -> expr t
+  ; binary_op_p : dispatch -> expr t
+  ; app_p : dispatch -> expr t
+  ; abs_p : dispatch -> expr t
+  ; if_then_else_p : dispatch -> expr t
+  ; let_in_p : dispatch -> expr t
   }
 
+(* declaration parser *)
+let declaration_p d =
+  fix
+  @@ fun self ->
+  let name keyword =
+    string keyword *> skip *> d.identifier_p
+    >>= function
+    | EIdentifier id -> return id
+    | _ -> fail "Invalid binding name"
+  in
+  let args = skip *> many (varname <* skip) in
+  let body =
+    skip
+    *> char '='
+    *> choice
+         [ d.literal_p
+         ; d.identifier_p
+         ; d.unary_op_p d
+         ; d.binary_op_p d
+         ; d.app_p d
+         ; d.abs_p d
+         ; d.if_then_else_p d
+         ; d.let_in_p d
+         ]
+  in
+  skip
+  *> (parens self <|> lift2 d_val (name "val") body <|> lift3 d_fun (name "fun") args body)
+;;
+
+(* expression parsers *)
 let literal_p =
   fix
   @@ fun self ->
-  skip
-  *>
   let boolean = string "true" <|> string "false" >>| bool_of_string in
   let integer =
     take_while1 (function
@@ -65,17 +98,15 @@ let literal_p =
       | _ -> false)
     >>| int_of_string
   in
-  let int_literal_p = integer >>| fun x -> LInt x in
-  let bool_literal_p = boolean >>| fun x -> LBool x in
-  let literal = choice [ int_literal_p; bool_literal_p ] in
-  parens self <|> lift e_literal literal
+  let int_literal = integer >>| fun x -> LInt x in
+  let bool_literal = boolean >>| fun x -> LBool x in
+  let literal = choice [ int_literal; bool_literal ] in
+  skip *> (parens self <|> lift e_literal literal)
 ;;
 
 let identifier_p =
   fix
   @@ fun self ->
-  skip
-  *>
   let is_wildcard c = c == "_" in
   let id =
     varname
@@ -83,27 +114,39 @@ let identifier_p =
     | id when not (is_wildcard id) -> return @@ e_identifier id
     | _ -> fail "Invalid variable name"
   in
-  parens self <|> id
+  skip *> (parens self <|> id)
 ;;
 
 let unary_op_p d =
   fix
   @@ fun self ->
-  skip
-  *>
+  parens self
+  <|>
   let op = choice [ char '~' >>| uneg; string "not" >>| unot ] in
-  parens self <|> lift2 e_unary_op op (d.expression_p d)
+  let body =
+    choice
+      [ d.literal_p
+      ; d.identifier_p
+      ; self
+      ; d.binary_op_p d
+      ; d.app_p d
+      ; d.abs_p d
+      ; d.if_then_else_p d
+      ; d.let_in_p d
+      ]
+  in
+  skip *> lift2 e_unary_op op body
 ;;
 
 let binary_op_p d =
   fix
   @@ fun self ->
-  skip
-  *>
-  let multiplicative = spaces *> choice [ char '*' >>| bmul; char '/' >>| bdiv ] in
-  let additive = spaces *> choice [ char '+' >>| badd; char '-' >>| bsub ] in
+  parens self
+  <|>
+  let multiplicative = skip *> choice [ char '*' >>| bmul; char '/' >>| bdiv ] in
+  let additive = skip *> choice [ char '+' >>| badd; char '-' >>| bsub ] in
   let relational =
-    spaces
+    skip
     *> choice
          [ string ">=" >>| bgte
          ; string "<=" >>| blte
@@ -111,9 +154,9 @@ let binary_op_p d =
          ; char '<' >>| blt
          ]
   in
-  let equality = spaces *> string "=" >>| beq in
-  let logical_and = spaces *> string "andalso" >>| band in
-  let logical_or = spaces *> string "orelse" >>| bor in
+  let equality = skip *> string "=" >>| beq in
+  let logical_and = skip *> string "andalso" >>| band in
+  let logical_or = skip *> string "orelse" >>| bor in
   let rec parse_bin_op expr_parser op_parsers =
     let chainl1 expr_p op_p =
       let rec go acc =
@@ -126,86 +169,122 @@ let binary_op_p d =
     | h :: t -> chainl1 (parse_bin_op expr_parser t) h
     | _ -> fail "Unreachable"
   in
-  parens self
-  <|> parse_bin_op
-        (d.expression_p d)
-        [ logical_or; logical_and; equality; relational; additive; multiplicative ]
+  let body =
+    choice
+      [ d.literal_p
+      ; d.identifier_p
+      ; d.unary_op_p d
+      ; self
+      ; d.app_p d
+      ; d.abs_p d
+      ; d.if_then_else_p d
+      ; d.let_in_p d
+      ]
+  in
+  skip
+  *> parse_bin_op
+       body
+       [ logical_or; logical_and; equality; relational; additive; multiplicative ]
 ;;
 
 let app_p d =
   fix
   @@ fun self ->
-  skip
-  *>
-  let expr = spaces *> d.expression_p d <* spaces in
-  parens self <|> lift2 e_app expr expr
+  parens self
+  <|>
+  let expr =
+    choice
+      [ d.literal_p
+      ; d.identifier_p
+      ; d.unary_op_p d
+      ; d.binary_op_p d
+      ; self
+      ; d.abs_p d
+      ; d.if_then_else_p d
+      ; d.let_in_p d
+      ]
+  in
+  skip *> lift2 e_app expr expr
 ;;
 
 let abs_p d =
   fix
   @@ fun self ->
-  skip
-  *>
-  let keyword = skip *> string "fn" *> spaces in
-  let arg = varname in
-  let body = spaces *> string "=>" *> d.expression_p d in
-  parens self <|> keyword *> lift2 e_abs arg body
+  parens self
+  <|>
+  let arg = skip *> string "fn" *> skip *> varname <* skip <* string "=>" in
+  let body =
+    choice
+      [ d.literal_p
+      ; d.identifier_p
+      ; d.unary_op_p d
+      ; d.binary_op_p d
+      ; d.app_p d
+      ; self
+      ; d.if_then_else_p d
+      ; d.let_in_p d
+      ]
+  in
+  skip *> lift2 e_abs arg body
 ;;
 
 let if_then_else_p d =
   fix
   @@ fun self ->
-  skip
-  *>
-  let cond = spaces *> string "if" *> d.expression_p d in
-  let if_true = spaces *> string "then" *> d.expression_p d in
-  let if_false = spaces *> string "else" *> d.expression_p d in
-  parens self <|> lift3 e_if_then_else cond if_true if_false
+  parens self
+  <|>
+  let expr =
+    choice
+      [ d.literal_p
+      ; d.identifier_p
+      ; d.unary_op_p d
+      ; d.binary_op_p d
+      ; d.app_p d
+      ; d.abs_p d
+      ; self
+      ; d.let_in_p d
+      ]
+  in
+  let cond = skip *> string "if" *> expr in
+  let if_true = skip *> string "then" *> expr in
+  let if_false = skip *> string "else" *> expr in
+  skip *> lift3 e_if_then_else cond if_true if_false
 ;;
 
 let let_in_p d =
   fix
   @@ fun self ->
-  skip
-  *>
-  let keyword = string "let" *> skip in
-  let declarations = sep_by1 spaces (d.declaration_p d) in
-  let body = skip *> string "in" *> d.expression_p d <* skip <* string "end" in
-  parens self <|> keyword *> lift2 e_let_in declarations body
-;;
-
-let expression_p d =
-  choice
-    [ literal_p
-    ; identifier_p
-    ; unary_op_p d
-    ; binary_op_p d
-    ; app_p d
-    ; abs_p d
-    ; if_then_else_p d
-    ; let_in_p d
-    ]
-;;
-
-(* declaration parser *)
-let declaration_p d =
-  fix
-  @@ fun self ->
-  skip
-  *>
-  let name keyword =
-    string keyword *> spaces *> identifier_p
-    >>= function
-    | EIdentifier id -> return id
-    | _ -> fail "Invalid binding name"
+  parens self
+  <|>
+  let expr =
+    choice
+      [ d.literal_p
+      ; d.identifier_p
+      ; d.unary_op_p d
+      ; d.binary_op_p d
+      ; d.app_p d
+      ; d.abs_p d
+      ; d.if_then_else_p d
+      ; self
+      ]
   in
-  let args = skip *> many (varname <* skip) in
-  let body = trim (char '=') *> d.expression_p d in
-  parens self <|> lift2 d_val (name "val") body <|> lift3 d_fun (name "fun") args body
+  let declarations = string "let" *> skip *> sep_by1 skip (declaration_p d) in
+  let body = skip *> string "in" *> expr <* skip <* string "end" in
+  skip *> lift2 e_let_in declarations body
 ;;
 
 (* main parser *)
-let dispatch = { expression_p; declaration_p }
+let dispatch =
+  { literal_p
+  ; identifier_p
+  ; unary_op_p
+  ; binary_op_p
+  ; app_p
+  ; abs_p
+  ; if_then_else_p
+  ; let_in_p
+  }
+;;
 
 let parse program =
   parse_string ~consume:All (many (declaration_p dispatch) <* skip) program
@@ -214,11 +293,7 @@ let parse program =
 let parse_optimistically program = Result.get_ok (parse program)
 
 (* tests *)
-let%test _ = parse_optimistically "val x = x" = [ DVal ("x", EIdentifier "x") ]
-
-(* let x program = parse_string ~consume:All varname program
-let _ =
-  x "x"
-  |> function
-  | Ok a | Error a -> print_string a
-;; *)
+let%test _ =
+  parse_optimistically "val x = (5 + 5)"
+  = [ DVal ("x", EBinaryOp (Add, ELiteral (LInt 5), ELiteral (LInt 5))) ]
+;;
