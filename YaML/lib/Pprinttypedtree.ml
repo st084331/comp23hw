681be9b0d70c87556b8d79883bddf6598f7cc5e4
Ilya Pankratov <i.pankratov.main@gmail.com>
2023-10-15 14:10:15 +0300

    Update pretty printer after updating typed tree
    Signed-off-by: Ilya Pankratov <i.pankratov.main@gmail.com>

--- YaML/lib/Pprinttypedtree.ml
+++ YaML/lib/Pprinttypedtree.ml
@@ -4,6 +4,7 @@
 
 open Typedtree
 open Base
+open Stdlib.Format
 
 let get_texpr_subst =
   let rec helper subs index = function
@@ -15,10 +16,11 @@ let get_texpr_subst =
       let subs, index = Pprinttypetree.get_ty_subs subs index typ in
       let subs, index = helper subs index e1 in
       helper subs index e2
-    | TLet (_, e, typ) | TLetRec (_, e, typ) | TFun (_, e, typ) ->
+    | TFun (_, e, typ) ->
       let subs, index = Pprinttypetree.get_ty_subs subs index typ in
       helper subs index e
-    | TIfThenElse (e1, e2, e3) ->
+    | TIfThenElse (e1, e2, e3, typ) ->
+      let subs, index = Pprinttypetree.get_ty_subs subs index typ in
       let subs, index = helper subs index e1 in
       let subs, index = helper subs index e2 in
       helper subs index e3
@@ -27,18 +29,16 @@ let get_texpr_subst =
   helper (Base.Map.empty (module Base.Int)) 0
 ;;
 
-let space ppf depth = Stdlib.Format.fprintf ppf "\n%*s" (4 * depth) ""
+let space ppf depth = fprintf ppf "\n%*s" (4 * depth) ""
 
 let pp_arg subs ppf =
   let pp_ty = Pprinttypetree.pp_ty_with_subs subs in
   function
-  | Arg (name, typ) -> Stdlib.Format.fprintf ppf "(%s: %a)" name pp_ty typ
+  | Arg (name, typ) -> fprintf ppf "(%s: %a)" name pp_ty typ
 ;;
 
 (* A monster that needs refactoring *)
-let pp_texpr ppf texpr =
-  let open Stdlib.Format in
-  let subs, _ = get_texpr_subst texpr in
+let pp_texpr_with_subs ppf depth subs texpr =
   let pp_ty = Pprinttypetree.pp_ty_with_subs (Some subs) in
   let pp_arg = pp_arg (Some subs) in
   let rec helper depth ppf =
@@ -78,21 +78,6 @@ let pp_texpr ppf texpr =
         e2
         space
         (depth - 1)
-    | TLet (name, e, typ) ->
-      fprintf
-        ppf
-        "(TLet(%a%s: %a, %a%a%a))"
-        space
-        depth
-        name
-        pp_ty
-        typ
-        space
-        depth
-        pp
-        e
-        space
-        (depth - 1)
     | TLetIn (name, e1, e2, typ) ->
       fprintf
         ppf
@@ -110,21 +95,6 @@ let pp_texpr ppf texpr =
         depth
         pp
         e2
-        space
-        (depth - 1)
-    | TLetRec (name, e, typ) ->
-      fprintf
-        ppf
-        "(TLetRec(%a%s: %a, %a%a%a))"
-        space
-        depth
-        name
-        pp_ty
-        typ
-        space
-        depth
-        pp
-        e
         space
         (depth - 1)
     | TLetRecIn (name, e1, e2, typ) ->
@@ -162,12 +132,14 @@ let pp_texpr ppf texpr =
         e
         space
         (depth - 1)
-    | TIfThenElse (i, t, e) ->
+    | TIfThenElse (i, t, e, typ) ->
       fprintf
         ppf
-        "(TIfThenElse(%a%a, %a%a, %a%a%a))"
+        "(TIfThenElse(%a : %a%a, %a%a, %a%a%a))"
         space
         depth
+        pp_ty
+        typ
         pp
         i
         space
@@ -182,5 +154,43 @@ let pp_texpr ppf texpr =
         (depth - 1)
     | TConst (c, typ) -> fprintf ppf "(TConst(%s: %a))" (Ast.show_const c) pp_ty typ
   in
-  helper 1 ppf texpr
+  helper depth ppf texpr
+;;
+
+let pp_texpr ppf texpr =
+  let subs, _ = get_texpr_subst texpr in
+  pp_texpr_with_subs ppf 1 subs
+;;
+
+let show_binding = function
+  | TLetRec _ -> "TLetRec"
+  | TLet _ -> "TLet"
+;;
+
+let pp_tbinding ppf = function
+  | (TLetRec (name, e, typ) | TLet (name, e, typ)) as binding ->
+    let subs, _ = get_texpr_subst e in
+    let pp_ty = Pprinttypetree.pp_ty_with_subs (Some subs) in
+    let pp_expr ppf = pp_texpr_with_subs ppf 2 subs in
+    fprintf
+      ppf
+      "(%s(%a%s: %a, %a%a%a))"
+      (show_binding binding)
+      space
+      1
+      name
+      pp_ty
+      typ
+      space
+      1
+      pp_expr
+      e
+      space
+      1
+;;
+
+let pp_statements =
+  pp_print_list
+    ~pp_sep:(fun ppf _ -> fprintf ppf ";")
+    (fun ppf binding -> pp_tbinding ppf binding)
 ;;
--- YaML/lib/Pprinttypedtree.mli
+++ YaML/lib/Pprinttypedtree.mli
@@ -2,5 +2,11 @@
 
 (** SPDX-License-Identifier: LGPL-2.1-or-later *)
 
-(** Pretty printer for typed expesstion *)
+(** Pretty printer for typed expession *)
 val pp_texpr : Format.formatter -> Typedtree.texpr -> unit
+
+(** Pretty printer for typed expession *)
+val pp_tbinding : Format.formatter -> Typedtree.tbinding -> unit
+
+(** Pretty printer for typed statements *)
+val pp_statements : Format.formatter -> Typedtree.tstatements -> unit
