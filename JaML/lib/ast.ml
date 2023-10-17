29ec4525948fe6dd872336fce3355ffe5bad1b68
Drumov Maxim <Drumov.M.A@gmail.com>
2023-10-17 18:20:23 +0300

    Change fun description
    Signed-off-by: Drumov Maxim <Drumov.M.A@gmail.com>

--- JaML/lib/ast.ml
+++ JaML/lib/ast.ml
@@ -36,7 +36,8 @@ type expr =
   | ELetIn of string * expr * expr
   (** An expression for let in declaration: let id = expr in expr *)
   | ELetRecIn of string * expr * expr
-  | EFun of string * expr
+  (** An expression for let rec in declaration: let rec id = expr in expr *)
+  | EFun of string * expr (** An expression for function: fun id -> expr *)
 [@@deriving show { with_path = false }]
 
 (** Binding type *)
