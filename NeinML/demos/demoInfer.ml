55837e494b4cf665f3e8ec74ac1e32d1826004d3
VyrodovMikhail <m_vyrodov@mail.ru>
2023-10-29 10:52:34 +0300

    Minor changes to pass CI

--- NeinML/demos/demoInfer.ml
+++ NeinML/demos/demoInfer.ml
@@ -3,6 +3,6 @@
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
 let () =
-  let s = Stdio.In_channel.input_all Caml.stdin in
+  let s = Stdio.In_channel.input_all Stdlib.stdin in
   Neinml_lib.Inferencer.parse_and_infer s
 ;;
