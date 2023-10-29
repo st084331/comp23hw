8a02155489d52bc3157e04b1573228afb0ff4925
VyrodovMikhail <m_vyrodov@mail.ru>
2023-10-29 11:00:15 +0300

    Delete deprecated lib usage

--- NeinML/demos/demoParse.ml
+++ NeinML/demos/demoParse.ml
@@ -5,7 +5,7 @@
 open Neinml_lib
 
 let () =
-  let s = Stdio.In_channel.input_all Caml.stdin in
+  let s = Stdio.In_channel.input_all Stdlib.stdin in
   match Neinml_lib.Parser.parse s with
   | Result.Ok ast -> Format.printf "%a\n%!" Ast.pp_statements_list ast
   | Error _ -> Format.printf "Parsing error"
