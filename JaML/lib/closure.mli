b332985a5173fdacf4184d0ee88dcf41622f122b
Drumov Maxim <Drumov.M.A@gmail.com>
2023-10-31 14:43:03 +0300

    Add closure mli
    Signed-off-by: Drumov Maxim <Drumov.M.A@gmail.com>

---
+++ JaML/lib/closure.mli
@@ -0,0 +1,6 @@
+(** Copyright 2023-2024, Ilya Pankratov, Maxim Drumov *)
+
+(** SPDX-License-Identifier: LGPL-2.1-or-later *)
+
+(** Closure *)
+val closure : Typedtree.tbinding list -> Typedtree.tbinding list
