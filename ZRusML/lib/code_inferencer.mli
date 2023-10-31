4827604541fb22f0dbe87b198fbeb47f613d45b2
st084331 <71183199+st084331@users.noreply.github.com>
2023-10-31 15:56:19 +0300

    Create code_inferencer.mli

---
+++ ZRusML/lib/code_inferencer.mli
@@ -0,0 +1,12 @@
+(** Copyright 2023-2024, Rustam Shangareev and Danil Yevdokimov*)
+
+(** SPDX-License-Identifier: LGPL-2.1 *)
+
+(** [inference code] performs type inference on the given [code].
+    If the code is correctly parsed and no type errors are found,
+    it prints the inferred types for each declaration.
+    If a type error is detected, it prints an error message indicating the location of the error.
+    If the code cannot be parsed, it prints "Parse error".
+    @param code The input code as a string.
+*)
+val inference : unit -> string -> unit
