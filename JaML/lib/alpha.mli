0afe64e927582a635e0c506e938525d1b53ab3f7
Drumov Maxim <Drumov.M.A@gmail.com>
2023-11-01 14:49:48 +0300

    Add alpha mli
    Signed-off-by: Drumov Maxim <Drumov.M.A@gmail.com>

---
+++ JaML/lib/alpha.mli
@@ -0,0 +1,6 @@
+(** Copyright 2023-2024, Ilya Pankratov, Maxim Drumov *)
+
+(** SPDX-License-Identifier: LGPL-2.1-or-later *)
+
+(** Alpha conversion of statements *)
+val alpha : Typedtree.tbinding list -> Typedtree.tbinding list
