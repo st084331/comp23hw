c7ac6ef74c498585ba73b1719d7d548b882958bf
Ilya Pankratov <i.pankratov.main@gmail.com>
2023-11-05 15:45:54 +0300

    Add documentation comments
    Signed-off-by: Ilya Pankratov <i.pankratov.main@gmail.com>

--- JaML/lib/anf.ml
+++ JaML/lib/anf.ml
@@ -3,39 +3,42 @@
 (** SPDX-License-Identifier: LGPL-2.1-or-later *)
 
 type immexpr =
-  | ImmNum of int
-  | ImmBool of bool
-  | ImmId of string
+  | ImmNum of int (** ..., -1, 0, 1, ... *)
+  | ImmBool of bool (** true, false *)
+  | ImmId of string (** identifiers or variables *)
 [@@deriving show { with_path = false }]
 
 type cexpr =
-  | CPlus of immexpr * immexpr
-  | CMinus of immexpr * immexpr
-  | CDivide of immexpr * immexpr
-  | CMultiply of immexpr * immexpr
-  | CXor of immexpr * immexpr
-  | CAnd of immexpr * immexpr
-  | COr of immexpr * immexpr
-  | CEq of immexpr * immexpr
-  | CNeq of immexpr * immexpr
-  | CGt of immexpr * immexpr
-  | CLt of immexpr * immexpr
-  | CGte of immexpr * immexpr
-  | CLte of immexpr * immexpr
-  | CApp of immexpr * immexpr
-  | CImmExpr of immexpr
+  | CPlus of immexpr * immexpr (** a + b *)
+  | CMinus of immexpr * immexpr (** a - b *)
+  | CDivide of immexpr * immexpr (** a / b *)
+  | CMultiply of immexpr * immexpr (** a * b *)
+  | CXor of immexpr * immexpr (** a ^ b *)
+  | CAnd of immexpr * immexpr (** a && b *)
+  | COr of immexpr * immexpr (** a || b *)
+  | CEq of immexpr * immexpr (** a = b *)
+  | CNeq of immexpr * immexpr (** a <> b *)
+  | CGt of immexpr * immexpr (** a > b *)
+  | CLt of immexpr * immexpr (** a < b *)
+  | CGte of immexpr * immexpr (** a >= b *)
+  | CLte of immexpr * immexpr (** a <= b *)
+  | CApp of immexpr * immexpr (** function arg *)
+  | CImmExpr of immexpr (** immexpr *)
 [@@deriving show { with_path = false }]
 
 type aexpr =
-  | ALet of string * cexpr * aexpr
-  | ALetRec of string * cexpr * aexpr
-  | AIfThenElse of aexpr * aexpr * aexpr
-  | ACEexpr of cexpr
+  | ALet of string * cexpr * aexpr (** let name = cexpr in aexpr *)
+  | ALetRec of string * cexpr * aexpr (** let rec name = cexpr in aexpr *)
+  | AIfThenElse of aexpr * aexpr * aexpr (** if aexpr1 then aexpr2 else aexpe3 *)
+  | ACEexpr of cexpr (** cexpr *)
 [@@deriving show { with_path = false }]
 
+(** Anf binding type (top level declarations) *)
 type anfexpr =
-  | AnfLet of string * string list * aexpr
+  | AnfLet of string * string list * aexpr (** let name [arg1, arg2, ... arg3] = aexpr *)
   | AnfLetRec of string * string list * aexpr
+  (** let rec name [arg1, arg2, ... arg3] = aexpr *)
 [@@deriving show { with_path = false }]
 
+(**Statements type (list of top level declarations) *)
 type anfstatements = anfexpr list [@@deriving show { with_path = false }]
