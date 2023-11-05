bdd5eec50370bcba49efffca99f1561363d13e08
Ilya Pankratov <i.pankratov.main@gmail.com>
2023-11-05 19:06:30 +0300

    Fix merge bug
    Signed-off-by: Ilya Pankratov <i.pankratov.main@gmail.com>

--- JaML/lib/alpha.ml
+++ JaML/lib/alpha.ml
@@ -19,18 +19,12 @@ let rec alpha_expr env = function
   | TIfThenElse (cond, e1, e2, ty) ->
     TIfThenElse (alpha_expr env cond, alpha_expr env e1, alpha_expr env e2, ty)
   | TLetRecIn (id, e1, e2, ty) ->
-    let new_env = extend_env id env in
-    TLetRecIn (find id new_env, alpha_expr env e1, alpha_expr new_env e2, ty)
+    let env' = extend_env id env in
+    TLetRecIn (find id env', alpha_expr env e1, alpha_expr env' e2, ty)
   | TLetIn (id, e1, e2, ty) ->
-    let new_env = extend_env id env in
-    TLetIn (find id new_env, alpha_expr env e1, alpha_expr new_env e2, ty)
+    let env' = extend_env id env in
+    TLetIn (find id env', alpha_expr env e1, alpha_expr env' e2, ty)
   | other -> other
-;;
-
-let test x y =
-  let x = x * x in
-  let y = y * y in
-  x * y
 ;;
 
 let alpha_bindings env = function
