dd6a97de9bca95f1a79719f67fa55351735f9712
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-11-06 20:12:38 +0300

    Delete other users compiler projects

--- CreeperML/test/test_parser.ml
+++
@@ -1,7 +0,0 @@
-open CreeperML.Parser_interface.ParserInterface
-open CreeperML.Parser_ast.ParserAst
-
-let () =
-  match from_channel stdin with
-  | Error msg -> Printf.printf "%s" msg
-  | Ok p -> pp_program Format.std_formatter p
--- NeinML/demos/demoInfer.ml
+++
@@ -1,8 +0,0 @@
-(** Copyright 2023-2024, Mikhail Vyrodov *)
-
-(** SPDX-License-Identifier: LGPL-3.0-or-later *)
-
-let () =
-  let s = Stdio.In_channel.input_all Stdlib.stdin in
-  Neinml_lib.Inferencer.parse_and_infer s
-;;
