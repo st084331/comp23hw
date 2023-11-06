1a294b5f61b73f78c23cf53cdc48b072c44eccff
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-11-06 20:10:54 +0300

    Cosmetics changes

--- ZRusML/lib/parser.ml
+++ ZRusML/lib/parser.ml
@@ -238,7 +238,7 @@ let pack =
     let eq_op = procl (choice_op [ "=", Eq; "<>", Neq ]) cmp_op @@ d.key d in
     let conj_op = procl (choice_op [ "&&", And ]) eq_op @@ d.key d in
     let disj_op = procl (choice_op [ "||", Or ]) conj_op @@ d.key d in
-    empty_lr @@ disj_op
+    empty_lr disj_op
   in
   { key; op; exp }
 ;;
