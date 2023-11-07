1e79ee6ad6e95129c16f6b723c407c07b571fcf6
Kalashnikov Matvey <kalashnikov.matvey.m@gmail.com>
2023-11-08 00:54:03 +0300

    Realized the need for an environment

--- SCAML/lib/closure.ml
+++ SCAML/lib/closure.ml
@@ -2,25 +2,45 @@ open Ast
 open Base
 open Parser
 
-let closure_conversion decl =
-  let rec expr_closure = function
-    | EConst x -> Set.empty (module String), constr_econst x
-    | EVar x -> Set.singleton (module String) x, constr_evar x
+let rec free_vars = function 
+| EConst _ -> Set.empty (module String)
+| EVar x -> Set.singleton (module String) x
+| EBinOp (_, e1, e2) -> Set.union (free_vars e1) (free_vars e2)
+| EIf (e1, e2, e3) -> Set.union (Set.union (free_vars e1) (free_vars e2)) (free_vars e3)
+| EApp (e1, e2) -> Set.union (free_vars e1) (free_vars e2)
+| EFun (x, e) -> (
+   let free = free_vars e in
+   match x with
+   | PVar id -> Set.remove free id
+   | _ -> free
+   )
+| ELetIn (b, x, e1, e2) -> (
+   let free1 = free_vars e1 in
+   let free2 = free_vars e2 in
+   let free1' = if b then Set.remove free1 x else free1 in
+   Set.union free1' (Set.remove free2 x)
+   )
+   
+let closure_conversion global_env decl =
+  let rec expr_closure local_env global_env = function
+    | EConst x -> constr_econst x
+    | EVar x -> constr_evar x
     | EBinOp (op, e1, e2) ->
-      let s1, e1' = expr_closure e1 in
-      let s2, e2' = expr_closure e2 in
-      Set.union s1 s2, constr_ebinop op e1' e2'
+      let e1' = expr_closure local_env global_env e1  in
+      let e2' = expr_closure local_env global_env e2  in
+      constr_ebinop op e1' e2'
     | EIf (e1, e2, e3) ->
-      let s1, e1' = expr_closure e1 in
-      let s2, e2' = expr_closure e2 in
-      let s3, e3' = expr_closure e3 in
-      Set.union (Set.union s1 s2) s3, constr_eif e1' e2' e3'
+      let e1' = expr_closure local_env global_env e1  in
+      let  e2' = expr_closure local_env global_env e2  in
+      let e3' = expr_closure local_env global_env e3  in
+      constr_eif e1' e2' e3'
     | EApp (e1, e2) ->
-      let s1, e1' = expr_closure e1 in
-      let s2, e2' = expr_closure e2 in
-      Set.union s1 s2, constr_eapp e1' [ e2' ]
+      let e1' = expr_closure local_env global_env e1  in
+      let e2' = expr_closure local_env global_env e2  in
+      constr_eapp e1' [e2']
     | EFun (x, _) as orig ->
-      let s, e' = efun_helper orig in
+      let s = free_vars orig in
+      let e' = efun_helper local_env global_env orig in
       (match x with
        | PVar _ ->
          let folded =
@@ -29,34 +49,33 @@ let closure_conversion decl =
          let folded2 =
            Set.fold ~init:folded ~f:(fun acc x -> constr_eapp acc [ constr_evar x ]) s
          in
-         Set.empty (module String), folded2
-       | _ -> Set.empty (module String), e')
+          folded2
+       | _ ->  e')
     | ELetIn (b, x, e1, e2) ->
-      let s1, e1' = efun_helper e1 in
-      let s2, e2' = expr_closure e2 in
-      let s1' = if b then Set.remove s1 x else s1 in
-      Set.union s1' (Set.remove s2 x), constr_eletin b x e1' e2'
-  and efun_helper = function
+      let e1' = efun_helper local_env global_env e1 in
+      let e2' = expr_closure local_env global_env e2 in
+      constr_eletin b x e1' e2' 
+  and efun_helper local_env global_env = function
     | EFun (x, e) ->
       (match x with
        | PVar id ->
-         let s, e' = efun_helper e in
-         Set.remove s id, constr_efun [ constr_pvar id ] e'
+         let e' = efun_helper local_env global_env e in
+         constr_efun [ constr_pvar id ] e'
        | p ->
-         let s, e' = efun_helper e in
-         s, constr_efun [ p ] e')
-    | expr -> expr_closure expr
+         let e' = efun_helper local_env global_env e in
+         constr_efun [ p ] e')
+    | expr -> expr_closure local_env global_env expr
   in
-  let decl_closure = function
-    | ELet (is_rec, id, e) -> constr_elet is_rec id (snd (expr_closure e))
+  let decl_closure global_env = function
+    | ELet (is_rec, id, e) -> constr_elet is_rec id (expr_closure (Map.empty (module Base.String)) global_env e)
   in
-  decl_closure decl
+  decl_closure global_env decl
 ;;
 
 let prog_conversion = List.map ~f:closure_conversion
 
 let print_prog_result decl =
-  let buf = closure_conversion decl in
+  let buf = closure_conversion (Set.empty (module Base.String)) decl in
   Stdlib.Format.printf "%s" (Ast.show_program [ buf ])
 ;;
 
@@ -169,8 +188,8 @@ let%expect_test _ =
    let rec fack n k = if n <= 1 then k 1 else fack (n - 1) (fack1 k ) in
    fack n (fun x -> x);; *)
 
-let a c d =
+(* let a c d =
   let m = c + d in
   let k l = l + m in
   k (5 + m)
-;;
+;; *)
