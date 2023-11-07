8aa6cdace6c939fbccabf24ab8aaa3490ff74e81
Kalashnikov Matvey <kalashnikov.matvey.m@gmail.com>
2023-11-08 02:41:18 +0300

    Closure conversion

--- SCAML/lib/closure.ml
+++ SCAML/lib/closure.ml
@@ -2,72 +2,86 @@ open Ast
 open Base
 open Parser
 
-let rec free_vars = function 
-| EConst _ -> Set.empty (module String)
-| EVar x -> Set.singleton (module String) x
-| EBinOp (_, e1, e2) -> Set.union (free_vars e1) (free_vars e2)
-| EIf (e1, e2, e3) -> Set.union (Set.union (free_vars e1) (free_vars e2)) (free_vars e3)
-| EApp (e1, e2) -> Set.union (free_vars e1) (free_vars e2)
-| EFun (x, e) -> (
-   let free = free_vars e in
-   match x with
-   | PVar id -> Set.remove free id
-   | _ -> free
-   )
-| ELetIn (b, x, e1, e2) -> (
-   let free1 = free_vars e1 in
-   let free2 = free_vars e2 in
-   let free1' = if b then Set.remove free1 x else free1 in
-   Set.union free1' (Set.remove free2 x)
-   )
-   
+let rec free_vars = function
+  | EConst _ -> Set.empty (module String)
+  | EVar x -> Set.singleton (module String) x
+  | EBinOp (_, e1, e2) -> Set.union (free_vars e1) (free_vars e2)
+  | EIf (e1, e2, e3) -> Set.union (Set.union (free_vars e1) (free_vars e2)) (free_vars e3)
+  | EApp (e1, e2) -> Set.union (free_vars e1) (free_vars e2)
+  | EFun (x, e) ->
+    let free = free_vars e in
+    (match x with
+     | PVar id -> Set.remove free id
+     | _ -> free)
+  | ELetIn (b, x, e1, e2) ->
+    let free1 = free_vars e1 in
+    let free2 = free_vars e2 in
+    let free1' = if b then Set.remove free1 x else free1 in
+    Set.union free1' (Set.remove free2 x)
+;;
+
 let closure_conversion global_env decl =
   let rec expr_closure local_env global_env = function
     | EConst x -> constr_econst x
-    | EVar x -> constr_evar x
+    | EVar x as orig ->
+      (match Map.find local_env x with
+       | Some free ->
+         Set.fold
+           ~init:orig
+           ~f:(fun expr x -> constr_eapp expr [ constr_evar x ])
+           free
+       | None -> orig)
     | EBinOp (op, e1, e2) ->
-      let e1' = expr_closure local_env global_env e1  in
-      let e2' = expr_closure local_env global_env e2  in
+      let e1' = expr_closure local_env global_env e1 in
+      let e2' = expr_closure local_env global_env e2 in
       constr_ebinop op e1' e2'
     | EIf (e1, e2, e3) ->
-      let e1' = expr_closure local_env global_env e1  in
-      let  e2' = expr_closure local_env global_env e2  in
-      let e3' = expr_closure local_env global_env e3  in
+      let e1' = expr_closure local_env global_env e1 in
+      let e2' = expr_closure local_env global_env e2 in
+      let e3' = expr_closure local_env global_env e3 in
       constr_eif e1' e2' e3'
     | EApp (e1, e2) ->
-      let e1' = expr_closure local_env global_env e1  in
-      let e2' = expr_closure local_env global_env e2  in
-      constr_eapp e1' [e2']
+      let e1' = expr_closure local_env global_env e1 in
+      let e2' = expr_closure local_env global_env e2 in
+      constr_eapp e1' [ e2' ]
     | EFun (x, _) as orig ->
       let s = free_vars orig in
       let e' = efun_helper local_env global_env orig in
       (match x with
        | PVar _ ->
-         let folded =
+         let fun_fold =
            Set.fold_right ~init:e' ~f:(fun x acc -> constr_efun [ constr_pvar x ] acc) s
          in
-         let folded2 =
-           Set.fold ~init:folded ~f:(fun acc x -> constr_eapp acc [ constr_evar x ]) s
+         let app_fold =
+           Set.fold ~init:fun_fold ~f:(fun acc x -> constr_eapp acc [ constr_evar x ]) s
          in
-          folded2
-       | _ ->  e')
-    | ELetIn (b, x, e1, e2) ->
+         app_fold
+       | _ -> e')
+    | ELetIn (b, x, (EFun _ as e1), e2) as orig ->
+      let free = free_vars orig in
+      let free' = Set.diff free global_env in
       let e1' = efun_helper local_env global_env e1 in
+      let local_env' = Map.set local_env ~key:x ~data:free' in
+      let e2' = expr_closure local_env' (Set.add global_env x) e2 in
+      constr_eletin b x e1' e2'
+    | ELetIn (b, x, e1, e2) ->
+      let e1' = expr_closure local_env global_env e1 in
       let e2' = expr_closure local_env global_env e2 in
-      constr_eletin b x e1' e2' 
+      constr_eletin b x e1' e2'
   and efun_helper local_env global_env = function
     | EFun (x, e) ->
       (match x with
-       | PVar id ->
+       | PVar _ as orig ->
          let e' = efun_helper local_env global_env e in
-         constr_efun [ constr_pvar id ] e'
+         constr_efun [ orig ] e'
        | p ->
          let e' = efun_helper local_env global_env e in
          constr_efun [ p ] e')
     | expr -> expr_closure local_env global_env expr
   in
   let decl_closure global_env = function
-    | ELet (is_rec, id, e) -> constr_elet is_rec id (expr_closure (Map.empty (module Base.String)) global_env e)
+    | ELet (is_rec, id, e) ->
+      constr_elet is_rec id (expr_closure (Map.empty (module Base.String)) global_env e)
   in
   decl_closure global_env decl
 ;;
@@ -141,20 +155,21 @@ let%expect_test _ =
           ))
         ] |}]
 ;;
-(* 
-let%expect_test _ =
-  print_prog_result
-    (ELet
-       ( false
-       , "a"
-       , EFun
-           ( PVar "c"
-           , ELetIn
-               ( false
-               , "k"
-               , EFun (PVar "m", EBinOp (Add, EVar "m", EVar "c"))
-               , EApp (EVar "k", EConst (CInt 5)) ) ) ));
-  [%expect
+
+(*
+   let%expect_test _ =
+   print_prog_result
+   (ELet
+   ( false
+   , "a"
+   , EFun
+   ( PVar "c"
+   , ELetIn
+   ( false
+   , "k"
+   , EFun (PVar "m", EBinOp (Add, EVar "m", EVar "c"))
+   , EApp (EVar "k", EConst (CInt 5)) ) ) ));
+   [%expect
     {|
       [(ELet (false, "a",
           (EFun ((PVar "c"),
@@ -169,7 +184,7 @@ let%expect_test _ =
           ))
         ]
      |}]
-;; *)
+   ;; *)
 
 (* let fac n =
    let rec fack n k =
@@ -188,8 +203,8 @@ let%expect_test _ =
    let rec fack n k = if n <= 1 then k 1 else fack (n - 1) (fack1 k ) in
    fack n (fun x -> x);; *)
 
-(* let a c d =
+let a c d =
   let m = c + d in
-  let k l = l + m in
+  let k _ = 1 + m in
   k (5 + m)
-;; *)
+;;
