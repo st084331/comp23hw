7ba0d2bd7a4cfa950664e38de884b85eb36338e3
Kalashnikov Matvey <kalashnikov.matvey.m@gmail.com>
2023-11-08 16:48:47 +0300

    tests and pretty printer

---
+++ SCAML/demos/closure_test/closureTests.ml
@@ -0,0 +1,20 @@
+open Scaml_lib.Parser
+open Scaml_lib.Closure
+open Scaml_lib.AstPrinter
+open Scaml_lib.Inferencer
+open Scaml_lib.Ty
+open Base
+
+let print_prog_result prog =
+  match parse prog with
+  | Ok prog ->
+    (match run_prog_inference prog with
+     | Ok _ ->
+       let prog_closure = prog_conversion prog in
+       List.iter prog_closure ~f:(fun binding ->
+         Stdlib.Format.printf "%a\n" pp_binding binding)
+     | Error e -> print_typ_err e)
+  | Error e -> Stdlib.Format.printf "Error%s" e
+;;
+
+let () = print_prog_result (Stdio.In_channel.input_all Stdlib.stdin)
