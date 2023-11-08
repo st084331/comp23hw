85a9af0d44f24704137431b4522cbb4a2933753f
st084331 <71183199+st084331@users.noreply.github.com>
2023-11-08 05:04:07 +0300

    StringSet implementation

--- ZRusML/lib/closure_conversion.ml
+++ ZRusML/lib/closure_conversion.ml
@@ -3,90 +3,87 @@
 (** SPDX-License-Identifier: LGPL-2.1 *)
 
 open Ast
+open Core
+module StringSet = Set.Make (String)
 
-let rec free_vars exp bound_vars =
+let rec find_free_vars exp bound_vars =
   match exp with
-  | EVar id -> if List.mem id bound_vars then [] else [ id ]
-  | EConst _ -> []
-  | EUnOp (_, e) -> free_vars e bound_vars
-  | EBinOp (_, e1, e2) -> List.append (free_vars e1 bound_vars) (free_vars e2 bound_vars)
-  | EIf (cond, e1, e2) ->
-    List.flatten
-      [ free_vars cond bound_vars; free_vars e1 bound_vars; free_vars e2 bound_vars ]
+  | EConst _ -> StringSet.empty
+  | EUnOp (_, e) -> find_free_vars e bound_vars
+  | EVar id -> if Set.mem bound_vars id then StringSet.empty else StringSet.singleton id
   | ELet (bindings, e) ->
-    let new_bound_vars, vars_from_bindings =
-      List.fold_left
-        (fun (bvars, fvars) (is_rec, pt, exp) ->
-          match pt with
-          | PtVar id ->
-            let new_bvars = if is_rec then id :: bvars else bvars in
-            new_bvars, List.append fvars (free_vars exp new_bvars)
-          | _ ->
-            failwith
-              "Pattern matching in let bindings is not supported in this free_vars \
-               function")
-        (bound_vars, [])
+    let bound_in_bindings, free_in_bindings =
+      List.fold_right
         bindings
+        ~init:(bound_vars, StringSet.empty)
+        ~f:(fun (is_rec, pat, exp) (bound, free) ->
+          let new_bound = if is_rec then Set.add bound (pattern_to_id pat) else bound in
+          let free_vars_in_exp = find_free_vars exp new_bound in
+          new_bound, Set.union free_vars_in_exp free)
     in
-    List.append vars_from_bindings (free_vars e new_bound_vars)
-  | EFun (pt, e) ->
-    (match pt with
-     | PtVar id -> free_vars e (id :: bound_vars)
-     | _ ->
-       failwith
-         "Pattern matching in functions is not supported in this free_vars function")
-  | EApp (e1, e2) -> List.append (free_vars e1 bound_vars) (free_vars e2 bound_vars)
-  | _ -> failwith "free_vars: expression not yet implemented for free variable analysis"
-;;
-
-let rec convert_exp exp bound_vars =
-  match exp with
-  | EConst _ | EVar _ -> exp
-  | EUnOp (op, e) -> EUnOp (op, convert_exp e bound_vars)
-  | EBinOp (op, e1, e2) ->
-    EBinOp (op, convert_exp e1 bound_vars, convert_exp e2 bound_vars)
-  | EIf (cond, e_then, e_else) ->
-    EIf
-      ( convert_exp cond bound_vars
-      , convert_exp e_then bound_vars
-      , convert_exp e_else bound_vars )
-  | ELet (bindings, e) ->
-    let bound_vars =
-      List.fold_left
-        (fun acc (is_rec, pt, _) ->
-          match pt with
-          | PtVar id -> id :: acc
-          | _ -> acc)
-        bound_vars
-        bindings
-    in
-    let bindings' =
-      List.map (fun (is_rec, pt, exp) -> is_rec, pt, convert_exp exp bound_vars) bindings
-    in
-    let e' = convert_exp e bound_vars in
-    ELet (bindings', e')
-  | EFun (PtVar id, body) ->
-    let fv = free_vars body [ id ] in
-    let closure_body = EFun (PtVar id, convert_exp body (id :: fv)) in
-    let closure =
-      ELet (List.map (fun var -> false, PtVar var, EVar var) fv, closure_body)
-    in
-    ELet ([ false, PtVar id, closure ], EVar id)
-  | EFun _ ->
-    failwith "Pattern matching in functions is not supported in this closure conversion"
+    Set.union (find_free_vars e bound_in_bindings) free_in_bindings
+  | EFun (pat, e) ->
+    let new_bound = Set.add bound_vars (pattern_to_id pat) in
+    find_free_vars e new_bound
+  | EIf (e1, e2, e3) ->
+    StringSet.union_list
+      [ find_free_vars e1 bound_vars
+      ; find_free_vars e2 bound_vars
+      ; find_free_vars e3 bound_vars
+      ]
+  | EBinOp (_, e1, e2) ->
+    Set.union (find_free_vars e1 bound_vars) (find_free_vars e2 bound_vars)
   | EApp (e1, e2) ->
-    let e1' = convert_exp e1 bound_vars in
-    let e2' = convert_exp e2 bound_vars in
-    EApp (e1', e2')
-  | _ -> failwith "convert_exp: expression not yet implemented for closure conversion"
+    Set.union (find_free_vars e1 bound_vars) (find_free_vars e2 bound_vars)
+
+and pattern_to_id = function
+  | PtVar id -> id
+  | _ -> failwith "pattern_to_id: pattern is not a variable"
 ;;
 
-let closure_convert_program decls =
-  let converted_decls =
-    List.map
-      (function
-        | DLet (is_rec, pt, exp) -> DLet (is_rec, pt, convert_exp exp []))
-      decls
-  in
-  converted_decls
+let rec closure_convert_exp env exp =
+  match exp with
+  | EVar id as v -> if Set.mem env id then EApp (EVar "closure", v) else v
+  | EFun (pat, e) ->
+    let new_env = Set.add env (pattern_to_id pat) in
+    let free_vars = find_free_vars e new_env in
+    if Set.is_empty free_vars
+    then EFun (pat, e)
+    else (
+      let closure_vars = Set.diff free_vars env in
+      let body = closure_convert_exp new_env e in
+      let body_with_bindings =
+        Set.fold
+          ~init:body
+          ~f:(fun acc var ->
+            if Set.mem env var
+            then acc
+            else ELet ([ false, PtVar var, EApp (EVar "closure", EVar var) ], acc))
+          closure_vars
+      in
+      EFun (PtVar "closure", EFun (pat, body_with_bindings)))
+  | ELet (bindings, e) ->
+    let new_env, new_bindings =
+      List.fold_right bindings ~init:(env, []) ~f:(fun (is_rec, pat, exp) (env, acc) ->
+        let new_env = if is_rec then Set.add env (pattern_to_id pat) else env in
+        let new_exp = closure_convert_exp new_env exp in
+        new_env, (is_rec, pat, new_exp) :: acc)
+    in
+    ELet (new_bindings, closure_convert_exp new_env e)
+  | EIf (e1, e2, e3) ->
+    EIf
+      (closure_convert_exp env e1, closure_convert_exp env e2, closure_convert_exp env e3)
+  | EBinOp (op, e1, e2) ->
+    EBinOp (op, closure_convert_exp env e1, closure_convert_exp env e2)
+  | EApp (e1, e2) -> EApp (closure_convert_exp env e1, closure_convert_exp env e2)
+  | _ -> exp
 ;;
+
+let closure_convert_prog prog =
+  let env = StringSet.empty in
+  List.map prog ~f:(function DLet (is_rec, pat, exp) ->
+    let exp' = closure_convert_exp env exp in
+    DLet (is_rec, pat, exp'))
+;;
+
+let closure_convert ast = closure_convert_prog ast
