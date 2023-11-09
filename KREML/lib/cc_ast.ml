2e9f9499e343b4f4d58e72acf4a1abf12e9e3541
Anton-Kraev <antonkraeww@gmail.com>
2023-11-10 01:26:26 +0300

    some modifications of ast

--- KREML/lib/cc_ast.ml
+++ KREML/lib/cc_ast.ml
@@ -1,7 +1,7 @@
 open Ast
 
 (** Simplified ast representation which is the result of closure conversion stage.
-    The only difference is that there's 1 binding in let_in expr, not a list *)
+    The difference is that there's 1 binding in let_in expr and uncurried lambda functions *)
 
 type cc_expr =
   | CLiteral of literal
@@ -9,27 +9,11 @@ type cc_expr =
   | CUnaryOp of unary_op * cc_expr
   | CBinaryOp of binary_op * cc_expr * cc_expr
   | CApp of cc_expr * cc_expr
-  | CAbs of identifier * cc_expr
+  | CAbs of identifier list * cc_expr
   | CIfThenElse of cc_expr * cc_expr * cc_expr
   | CLetIn of cc_binding * cc_expr
 
 and cc_binding =
-  | CVal of identifier * cc_expr (* val x = 88 *)
-  | CFun of identifier * identifier list * cc_expr (* fun sqr x = x * x *)
+  | CVal of identifier * cc_expr
+  | CFun of identifier * identifier list * cc_expr
 [@@deriving show { with_path = false }]
-
-(* smart constructors *)
-
-(* expressions *)
-let cc_literal x = CLiteral x
-let cc_identifier x = CIdentifier x
-let cc_unary_op op x = CUnaryOp (op, x)
-let cc_app x1 x2 = CApp (x1, x2)
-let cc_abs arg body = CAbs (arg, body)
-let cc_if_then_else cond if_true if_false = CIfThenElse (cond, if_true, if_false)
-let cc_binary_op op left right = CBinaryOp (op, left, right)
-let cc_let_in binding body = CLetIn (binding, body)
-
-(* bindings *)
-let cc_val value_id expression = CVal (value_id, expression)
-let cc_fun fun_id args body = CFun (fun_id, args, body)
--- KREML/lib/ll_ast.ml
+++ KREML/lib/ll_ast.ml
@@ -1,7 +1,7 @@
 open Ast
 
 (** Simplified ast representation which is the result of lambda lifting stage.
-    The difference with cc_expr is that there are no let_in expr and anonymous functions *)
+    The difference with cc_expr is that there are no nested and anonymous functions *)
 
 type ll_expr =
   | LLiteral of literal
@@ -10,23 +10,10 @@ type ll_expr =
   | LBinaryOp of binary_op * ll_expr * ll_expr
   | LApp of ll_expr * ll_expr
   | LIfThenElse of ll_expr * ll_expr * ll_expr
+  | LLetIn of identifier * ll_expr * ll_expr
 [@@deriving show { with_path = false }]
 
 type ll_binding =
-  | LVal of identifier * ll_expr (* val x = 88 *)
-  | LFun of identifier * identifier list * ll_expr (* fun sqr x = x * x *)
+  | LVal of identifier * ll_expr
+  | LFun of identifier * identifier list * ll_expr
 [@@deriving show { with_path = false }]
-
-(* smart constructors *)
-
-(* expressions *)
-let ll_literal x = LLiteral x
-let ll_identifier x = LIdentifier x
-let ll_unary_op op x = LUnaryOp (op, x)
-let ll_app x1 x2 = LApp (x1, x2)
-let ll_if_then_else cond if_true if_false = LIfThenElse (cond, if_true, if_false)
-let ll_binary_op op left right = LBinaryOp (op, left, right)
-
-(* bindings *)
-let ll_val value_id expression = LVal (value_id, expression)
-let ll_fun fun_id args body = LFun (fun_id, args, body)
