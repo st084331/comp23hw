7950b4fd8a513b318848d8294d22859710244d5c
Anton-Kraev <antonkraeww@gmail.com>
2023-11-10 18:43:32 +0300

    implemented closure conversion

--- KREML/lib/clos_conv.ml
+++ KREML/lib/clos_conv.ml
@@ -63,72 +63,110 @@ let get_function_names env =
   Set.of_list (module String) (List.map env ~f:get_name)
 ;;
 
-let rec conv_closure_binding env = function
-  | CVal (id, body) ->
-    let cc_body, env' = conv_closure_expr env body in
-    CVal (id, cc_body), env'
-  | CFun _ -> failwith ""
+let replace_id_by_expr where id expr =
+  match where with
+  | CIdentifier x when id == x -> expr
+  | e -> e
+;;
 
-and conv_closure_expr env = function
+let rec clos_conv env parent_scope = function
   | CIdentifier x -> CIdentifier x, env
   | CLiteral x -> CLiteral x, env
   | CUnaryOp (op, body) ->
-    let cc_body, env' = conv_closure_expr env body in
+    let cc_body, env' = clos_conv env parent_scope body in
     CUnaryOp (op, cc_body), env'
   | CBinaryOp (op, left, right) ->
-    let cc_left, env' = conv_closure_expr env left in
-    let cc_right, env'' = conv_closure_expr env' right in
+    let cc_left, env' = clos_conv env parent_scope left in
+    let cc_right, env'' = clos_conv env' parent_scope right in
     CBinaryOp (op, cc_left, cc_right), env''
   | CIfThenElse (cond, if_true, if_false) ->
-    let cc_cond, env' = conv_closure_expr env cond in
-    let cc_if_true, env'' = conv_closure_expr env' if_true in
-    let cc_if_false, env''' = conv_closure_expr env'' if_false in
+    let cc_cond, env' = clos_conv env parent_scope cond in
+    let cc_if_true, env'' = clos_conv env' parent_scope if_true in
+    let cc_if_false, env''' = clos_conv env'' parent_scope if_false in
     CIfThenElse (cc_cond, cc_if_true, cc_if_false), env'''
   | CApp (left, right) ->
-    let cc_left, env' = conv_closure_expr env left in
-    let cc_right, env'' = conv_closure_expr env' right in
+    let cc_left, env' = clos_conv env parent_scope left in
+    let cc_right, env'' = clos_conv env' parent_scope right in
     CApp (cc_left, cc_right), env''
   | CAbs (args, body) ->
-    let cc_body, env' = conv_closure_expr env body in
+    let cc_body, env' = clos_conv env parent_scope body in
     let closed_args = Set.diff (free_vars cc_body) (get_function_names env') in
     let cc_abs, env'' =
       match Set.to_list closed_args with
       | [] -> CAbs (args, cc_body), env'
       | closed_args ->
         let closure = create_closure closed_args args cc_body in
-        let env'' = closure :: env' in
-        let cc_abs = apply_closure closure closed_args in
-        cc_abs, env''
+        apply_closure closure closed_args, closure :: env'
     in
     cc_abs, env''
-  | CLetIn (binding, body) ->
-    let cc_binding, env' = conv_closure_binding env binding in
-    let cc_letin_body, env'' = conv_closure_expr env' body in
-    CLetIn (cc_binding, cc_letin_body), env''
+  | CLetIn (binding, letin_body) ->
+    (match binding with
+     | CVal (id, body) ->
+       let cc_body, env' = clos_conv env parent_scope body in
+       let closed_args =
+         Set.diff
+           (free_vars cc_body)
+           (Set.union
+              (Set.of_list (module String) parent_scope)
+              (get_function_names env'))
+       in
+       (match Set.to_list closed_args with
+        | [] ->
+          let cc_letin_body, env'' = clos_conv env' (id :: parent_scope) letin_body in
+          CLetIn (CVal (id, cc_body), cc_letin_body), env''
+        | closed_args ->
+          let closure = create_closure closed_args [] cc_body in
+          let applied_closure = apply_closure closure closed_args in
+          let cc_body = replace_id_by_expr cc_body id applied_closure in
+          let cc_letin_body, env'' =
+            clos_conv (closure :: env') (id :: parent_scope) letin_body
+          in
+          let cc_letin_body = replace_id_by_expr cc_letin_body id applied_closure in
+          CLetIn (CFun (id, closed_args, cc_body), cc_letin_body), env'')
+     | CFun (id, args, body) ->
+       let cc_body, env' = clos_conv env parent_scope body in
+       let closed_args =
+         Set.diff
+           (free_vars cc_body)
+           (Set.union
+              (Set.of_list (module String) parent_scope)
+              (Set.add (get_function_names env') id))
+       in
+       (match Set.to_list closed_args with
+        | [] ->
+          let cc_letin_body, env'' = clos_conv env' (id :: parent_scope) letin_body in
+          CLetIn (CFun (id, args, cc_body), cc_letin_body), env''
+        | closed_args ->
+          let closure = create_closure closed_args args cc_body in
+          let applied_closure = apply_closure closure closed_args in
+          let cc_body = replace_id_by_expr cc_body id applied_closure in
+          let cc_letin_body, env'' =
+            clos_conv (closure :: env') (id :: parent_scope) letin_body
+          in
+          let cc_letin_body = replace_id_by_expr cc_letin_body id applied_closure in
+          CLetIn (CFun (id, List.append closed_args args, cc_body), cc_letin_body), env''))
 ;;
 
-(*
-   val res = let fun f x y = (fn z => x + y + z) 1 in f end
+let get_name = function
+  | CVal (id, _) | CFun (id, _, _) -> id
+;;
 
-   fun cc_1 x y z = x + y + z
-   val res = let fun f x y = (cc1 x y) 1 in f end
-
-   val res = let fun f x y =
-   let fun g z = x + y + z
-   in g 1
-   in f end
-
-   fun g x y z = x + y + z
-   val res = let fun f x y = g 1
-   in f end
-
-   val res = let fun f x y =
-   let fun g z = (fn w => g w) x
-   in g 1
-   in f end
-
-   fun cc_1 g w = g w
-   fun g x y z = cc_1 g
-   val res = let fun f x y = g 1
-   in f end
-*)
+let clos_conv_program program =
+  let rec helper acc parent_scope = function
+    | [] -> acc
+    | CVal (id, body) :: tl ->
+      let cc_body, env = clos_conv [] parent_scope body in
+      helper
+        (CVal (id, List.fold_right env ~f:(fun l r -> CLetIn (l, r)) ~init:cc_body) :: acc)
+        (id :: parent_scope)
+        tl
+    | CFun (id, args, body) :: tl ->
+      let cc_body, env = clos_conv [] (id :: parent_scope) body in
+      helper
+        (CFun (id, args, List.fold_right env ~f:(fun l r -> CLetIn (l, r)) ~init:cc_body)
+         :: acc)
+        (id :: parent_scope)
+        tl
+  in
+  List.rev (helper [] [] (simplify program))
+;;
