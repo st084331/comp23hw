ca50fff0ddedf30cd99e3f5cc9c0a1bb09dec95f
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-11-15 22:01:04 +0300

    Remove helper functions from pretty_printer.mli

--- ZRusML/lib/pretty_printer.mli
+++ ZRusML/lib/pretty_printer.mli
@@ -1,3 +1,7 @@
+(** Copyright 2023-2024, Rustam Shangareev and Danil Yevdokimov *)
+
+(** SPDX-License-Identifier: LGPL-2.1 *)
+
 val show_const : Ast.const -> string
 val pp_const : Format.formatter -> Ast.const -> unit
 val show_bin_op : Ast.bin_op -> string
@@ -6,15 +10,9 @@ val show_un_op : Ast.un_op -> string
 val pp_un_op : Format.formatter -> Ast.un_op -> unit
 val show_pt : Ast.pt -> string
 val pp_pt : Format.formatter -> Ast.pt -> unit
-val sprintf_tabs : int -> string
-val print_tabs : Format.formatter -> int -> unit
-val show_exp_helper : int -> Ast.exp -> string
 val show_exp : Ast.exp -> string
 val pp_exp : Format.formatter -> Ast.exp -> unit
-val show_binding_helper : int -> bool * Ast.pt * Ast.exp -> string
 val show_binding : bool * Ast.pt * Ast.exp -> string
-val pp_binding_helper :
-  Format.formatter -> int -> bool * Ast.pt * Ast.exp -> unit
 val pp_binding : Format.formatter -> bool * Ast.pt * Ast.exp -> unit
 val show_decl : Ast.decl -> string
 val pp_decl : Format.formatter -> Ast.decl -> unit
