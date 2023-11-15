d59c50c9350b2e6c2b5b9d3643db894b106c5c24
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-11-15 20:45:47 +0300

    Fix code style and some changes in pretty_printer

--- ZRusML/lib/pretty_printer.mli
+++ ZRusML/lib/pretty_printer.mli
@@ -1,30 +1,22 @@
-(** Copyright 2023-2024, Rustam Shangareev and Danil Yevdokimov *)
-
-(** SPDX-License-Identifier: LGPL-2.1 *)
-
-(** By formatter print pretty formatted Ast.const *)
+val show_const : Ast.const -> string
 val pp_const : Format.formatter -> Ast.const -> unit
-
-(** By formatter print pretty formatted Ast.bin_op *)
+val show_bin_op : Ast.bin_op -> string
 val pp_bin_op : Format.formatter -> Ast.bin_op -> unit
-
-(** By formatter print pretty formatted Ast.un_op *)
+val show_un_op : Ast.un_op -> string
 val pp_un_op : Format.formatter -> Ast.un_op -> unit
-
-(** By formatter print pretty formatted Ast.pt *)
+val show_pt : Ast.pt -> string
 val pp_pt : Format.formatter -> Ast.pt -> unit
-
-(** By formatter and count print count tabs *)
+val sprintf_tabs : int -> string
 val print_tabs : Format.formatter -> int -> unit
-
-(** By formatter print pretty formatted Ast.const *)
-val pp_exp : Format.formatter -> int -> Ast.exp -> unit
-
-(** By formatter print pretty formatted Ast.const *)
-val pp_binding : Format.formatter -> int -> bool * Ast.pt * Ast.exp -> unit
-
-(** By formatter print pretty formatted Ast.const *)
+val show_exp_helper : int -> Ast.exp -> string
+val show_exp : Ast.exp -> string
+val pp_exp : Format.formatter -> Ast.exp -> unit
+val show_binding_helper : int -> bool * Ast.pt * Ast.exp -> string
+val show_binding : bool * Ast.pt * Ast.exp -> string
+val pp_binding_helper :
+  Format.formatter -> int -> bool * Ast.pt * Ast.exp -> unit
+val pp_binding : Format.formatter -> bool * Ast.pt * Ast.exp -> unit
+val show_decl : Ast.decl -> string
 val pp_decl : Format.formatter -> Ast.decl -> unit
-
-(** By formatter print pretty formatted Ast.decl list *)
+val show_prog : Ast.decl list -> string
 val pp_prog : Format.formatter -> Ast.decl list -> unit
