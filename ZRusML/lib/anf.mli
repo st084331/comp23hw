8741303a7035d7e85f58b1a03e676b24f5efcd82
st084331 <71183199+st084331@users.noreply.github.com>
2023-11-16 03:46:08 +0300

    error monad

--- ZRusML/lib/anf.mli
+++ ZRusML/lib/anf.mli
@@ -6,16 +6,11 @@
 
 open Ast
 
-(* Generate a fresh variable name. *)
+type error =
+  | NotImplemented
+  | OtherError of string
+
 val fresh_var : unit -> string
-
-(* Transform an expression into A-Normal Form (ANF),
-   returning the transformed expression along with any necessary let-bindings. *)
-val exp_to_anf : exp -> exp * (bool * pt * exp) list
-
-(* Transform a list of bindings into ANF,
-   returning the transformed bindings along with any necessary let-bindings. *)
-val bindings_to_anf : binding list -> exp * (bool * pt * exp) list
-
-(** Resets the counter used for generating fresh variables. *)
+val exp_to_anf : exp -> (exp * (bool * pt * exp) list, error) result
+val bindings_to_anf : binding list -> (exp * (bool * pt * exp) list, error) result
 val reset_counter : unit -> unit
