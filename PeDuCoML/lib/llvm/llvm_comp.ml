af32851fb5f62849119fa22d64e61c4d8585c1d1
Danila-Pechenev <danil-pechenev@mail.ru>
2023-11-20 20:15:33 +0300

    Add correct list and tuple compilation

--- PeDuCoML/lib/llvm/llvm_comp.ml
+++ PeDuCoML/lib/llvm/llvm_comp.ml
@@ -7,20 +7,16 @@ open Ast
 open Llvm
 
 let context = global_context ()
-let the_module = create_module context "PeDuCoML"
 let builder = builder context
+let the_module = create_module context "PeDuCoML"
 let i64 = i64_type context
 let lookup_function_exn id llmodule = Option.get @@ lookup_function id llmodule
-(* let build_call fn args name = build_call2 (type_of fn) fn args name builder *)
-(* let void = void_type context *)
-(* let i64_array = array_type i64 *)
-
-(* let create_entry_block_alloca func var =
-   let builder = Llvm.builder_at context (Llvm.instr_begin (Llvm.entry_block func)) in
-   Llvm.build_alloca i64 var builder
-   ;; *)
 
 type llvm_error = UnboundVariable of unique_id
+
+(* Smart constructors *)
+let unbound id = UnboundVariable id
+(* ------------------ *)
 
 open Result
 
@@ -35,20 +31,54 @@ let rec codegen_immexpr env = function
   | ImmInt num -> ok @@ const_int i64 num
   | ImmString str -> ok @@ const_string context str
   | ImmChar c -> ok @@ const_int i64 (Base.Char.to_int c)
-  | ImmBool v -> ok @@ const_int i64 (Bool.to_int v)
+  | ImmBool v -> ok @@ const_int i64 (Base.Bool.to_int v)
   | ImmId id ->
     (match Base.Map.Poly.find env id, id with
      | _, GlobalScopeId id -> ok @@ lookup_function_exn id the_module
-     | None, _ -> error @@ UnboundVariable id
-     | Some llvalue, AnfId id -> ok @@ build_load i64 llvalue (string_of_int id) builder)
-  | ImmList imm_list | ImmTuple imm_list ->
-    let* arr =
-      Base.List.fold_right imm_list ~init:(ok []) ~f:(fun imm acc ->
-        let* acc = acc in
-        let* imm = codegen_immexpr env imm in
-        ok @@ (imm :: acc))
+     | None, _ -> error @@ unbound id
+     | Some llvalue, AnfId id ->
+       ok @@ build_load i64 llvalue ("AnfId" ^ string_of_int id ^ "_n") builder)
+  | ImmList imm_list ->
+    let rec helper list_ptr_llv =
+      let add = lookup_function_exn "peducoml_add_to_list" the_module in
+      let fnty = function_type i64 [| i64; i64 |] in
+      function
+      | head :: tail ->
+        let* elem = codegen_immexpr env head in
+        helper
+          (build_call fnty add [| list_ptr_llv; elem |] "peducoml_add_to_list_n" builder)
+          tail
+      | _ -> ok list_ptr_llv
     in
-    ok @@ const_array i64 (Base.List.to_array arr)
+    let alloc_list = lookup_function_exn "peducoml_alloc_list" the_module in
+    let fnty = function_type i64 [||] in
+    let allocated_list =
+      build_call fnty alloc_list [||] "peducoml_alloc_list_n" builder
+    in
+    helper allocated_list (Base.List.rev imm_list)
+  | ImmTuple imm_list ->
+    let rec helper tuple_ptr_llv =
+      let fill = lookup_function_exn "peducoml_fill_tuple" the_module in
+      let fnty = function_type i64 [| i64; i64 |] in
+      function
+      | head :: tail ->
+        let* elem = codegen_immexpr env head in
+        helper
+          (build_call fnty fill [| tuple_ptr_llv; elem |] "peducoml_fill_tuple_n" builder)
+          tail
+      | _ -> ok tuple_ptr_llv
+    in
+    let alloc_tuple = lookup_function_exn "peducoml_alloc_tuple" the_module in
+    let fnty = function_type i64 [| i64 |] in
+    let allocated_tuple =
+      build_call
+        fnty
+        alloc_tuple
+        [| Base.List.length imm_list |> const_int i64 |]
+        "peducoml_alloc_tuple_n"
+        builder
+    in
+    helper allocated_tuple imm_list
 ;;
 
 let build_binary_operation = function
@@ -71,8 +101,15 @@ let codegen_cexpr env = function
   | CBinaryOperation (bop, left, right) ->
     let* left = codegen_immexpr env left in
     let* right = codegen_immexpr env right in
-    let rez = build_binary_operation bop left right "boptmp" builder in
-    let rez = build_zext rez i64 "zext" builder in
+    let result =
+      build_binary_operation
+        bop
+        left
+        right
+        (show_binary_operator bop ^ "result_n")
+        builder
+    in
+    let rez = build_zext result i64 "zext_n" builder in
     ok @@ rez
   | CApplication (func, arg) ->
     let* callee = codegen_immexpr env func in
@@ -81,23 +118,23 @@ let codegen_cexpr env = function
     if arity == 1
     then (
       let fnty = function_type i64 [| i64 |] in
-      ok @@ build_call fnty callee [| arg |] "tmp_call1" builder)
+      ok @@ build_call fnty callee [| arg |] "function_call_n" builder)
     else (
       let func_ptr =
         let alloc_closure = lookup_function_exn "peducoml_alloc_closure" the_module in
-        let alloc_ty = function_type i64 [| i64; i64 |] in
+        let fnty = function_type i64 [| i64; i64 |] in
         build_call
-          alloc_ty
+          fnty
           alloc_closure
-          [| build_pointercast callee i64 "funcptr" builder
+          [| build_pointercast callee i64 "ptr_to_i64_n" builder
            ; params callee |> Base.Array.length |> const_int i64
           |]
-          "peducoml_alloc_closure"
+          "peducoml_alloc_closure_n"
           builder
       in
       let apply = lookup_function_exn "peducoml_apply" the_module in
-      let apply_ty = function_type i64 [| i64; i64 |] in
-      ok @@ build_call apply_ty apply [| func_ptr; arg |] "peducoml_apply" builder)
+      let fnty = function_type i64 [| i64; i64 |] in
+      ok @@ build_call fnty apply [| func_ptr; arg |] "peducoml_apply_n" builder)
   | _ -> failwith "TODO"
 ;;
 
@@ -153,6 +190,19 @@ let codegen program =
       (function_type i64 [| i64; i64 |])
       the_module
     :: declare_function "peducoml_apply" (function_type i64 [| i64; i64 |]) the_module
+    :: declare_function "peducoml_alloc_list" (function_type i64 [||]) the_module
+    :: declare_function
+         "peducoml_add_to_list"
+         (function_type i64 [| i64; i64 |])
+         the_module
+    :: declare_function "peducoml_field" (function_type i64 [| i64; i64 |]) the_module
+    :: declare_function "peducoml_tail" (function_type i64 [| i64 |]) the_module
+    :: declare_function "peducoml_length" (function_type i64 [| i64 |]) the_module
+    :: declare_function "peducoml_alloc_tuple" (function_type i64 [| i64 |]) the_module
+    :: declare_function
+         "peducoml_fill_tuple"
+         (function_type i64 [| i64; i64 |])
+         the_module
     :: env
   in
   let rec codegen acc env = function
