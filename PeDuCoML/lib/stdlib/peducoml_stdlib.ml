baec804ca2b466f88d729791996f3ac0fc316199
Danila-Pechenev <danil-pechenev@mail.ru>
2023-11-20 20:16:01 +0300

    Add some stdlib functions

--- PeDuCoML/lib/stdlib/peducoml_stdlib.ml
+++ PeDuCoML/lib/stdlib/peducoml_stdlib.ml
@@ -8,10 +8,17 @@ let print_int_scheme = Base.Set.empty (module Base.Int), TArr (TGround Int, TGro
 let print_char_scheme = Base.Set.empty (module Base.Int), TArr (TGround Char, TGround Int)
 let print_bool_scheme = Base.Set.empty (module Base.Int), TArr (TGround Bool, TGround Int)
 
-(* let print_bool_scheme = Base.Set.empty (module Base.Int), TArr (TGround Bool, TGround Int) *)
+let print_list_scheme =
+  Base.Set.empty (module Base.Int), TArr (TList (TVar (-1)), TGround Int)
+;;
+
+let print_tuple_scheme = Base.Set.empty (module Base.Int), TArr (TVar (-1), TGround Int)
+
 let stdlib =
   [ "print_int", print_int_scheme
   ; "print_char", print_char_scheme
   ; "print_bool", print_bool_scheme
+  ; "print_list", print_list_scheme
+  ; "print_tuple", print_tuple_scheme
   ]
 ;;
