b6f0fb094ba4407b506017d5144c591101b18b6d
Danila-Pechenev <danil-pechenev@mail.ru>
2023-11-21 14:27:32 +0300

    Process all remaining ANF cases

--- PeDuCoML/lib/llvm/llvm_comp.ml
+++ PeDuCoML/lib/llvm/llvm_comp.ml
@@ -28,23 +28,24 @@ let string_of_unique_id = function
 ;;
 
 let rec codegen_immexpr env = function
-  | ImmInt num -> ok @@ const_int i64 num
-  | ImmString str -> ok @@ const_string context str
-  | ImmChar c -> ok @@ const_int i64 (Base.Char.to_int c)
-  | ImmBool v -> ok @@ const_int i64 (Base.Bool.to_int v)
+  (* bool value means whether the function returned global scope function *)
+  | ImmInt num -> ok @@ const_int i64 num, false
+  | ImmString str -> ok @@ const_string context str, false
+  | ImmChar c -> ok @@ const_int i64 (Base.Char.to_int c), false
+  | ImmBool v -> ok @@ const_int i64 (Base.Bool.to_int v), false
   | ImmId id ->
     (match Base.Map.Poly.find env id, id with
-     | _, GlobalScopeId id -> ok @@ lookup_function_exn id the_module
-     | None, _ -> error @@ unbound id
+     | _, GlobalScopeId id -> ok @@ lookup_function_exn id the_module, true
+     | None, _ -> error @@ unbound id, false
      | Some llvalue, AnfId id ->
-       ok @@ build_load i64 llvalue (string_of_int id ^ "_n") builder)
+       ok @@ build_load i64 llvalue (string_of_int id) builder, false)
   | ImmList imm_list ->
     let rec helper list_ptr_llv =
       let add = lookup_function_exn "peducoml_add_to_list" the_module in
       let fnty = function_type i64 [| i64; i64 |] in
       function
       | head :: tail ->
-        let* elem = codegen_immexpr env head in
+        let* elem = fst @@ codegen_immexpr env head in
         helper
           (build_call fnty add [| list_ptr_llv; elem |] "peducoml_add_to_list_n" builder)
           tail
@@ -55,14 +56,14 @@ let rec codegen_immexpr env = function
     let allocated_list =
       build_call fnty alloc_list [||] "peducoml_alloc_list_n" builder
     in
-    helper allocated_list (Base.List.rev imm_list)
+    helper allocated_list (Base.List.rev imm_list), false
   | ImmTuple imm_list ->
     let rec helper tuple_ptr_llv =
       let fill = lookup_function_exn "peducoml_fill_tuple" the_module in
       let fnty = function_type i64 [| i64; i64 |] in
       function
       | head :: tail ->
-        let* elem = codegen_immexpr env head in
+        let* elem = fst @@ codegen_immexpr env head in
         helper
           (build_call fnty fill [| tuple_ptr_llv; elem |] "peducoml_fill_tuple_n" builder)
           tail
@@ -78,7 +79,7 @@ let rec codegen_immexpr env = function
         "peducoml_alloc_tuple_n"
         builder
     in
-    helper allocated_tuple imm_list
+    helper allocated_tuple imm_list, false
 ;;
 
 let build_binary_operation = function
@@ -96,11 +97,16 @@ let build_binary_operation = function
   | OR -> build_or
 ;;
 
+let build_unary_operation = function
+  | Minus -> build_neg
+  | Not -> build_not
+;;
+
 let codegen_cexpr env = function
-  | CImm imm_expr -> codegen_immexpr env imm_expr
+  | CImm imm_expr -> fst @@ codegen_immexpr env imm_expr
   | CBinaryOperation (bop, left, right) ->
-    let* left = codegen_immexpr env left in
-    let* right = codegen_immexpr env right in
+    let* left = fst @@ codegen_immexpr env left in
+    let* right = fst @@ codegen_immexpr env right in
     let result =
       build_binary_operation
         bop
@@ -110,10 +116,18 @@ let codegen_cexpr env = function
         builder
     in
     let result = build_zext result i64 "zext_n" builder in
-    ok @@ result
+    ok result
+  | CUnaryOperation (uop, arg) ->
+    let* arg = fst @@ codegen_immexpr env arg in
+    let result =
+      build_unary_operation uop arg (show_unary_operator uop ^ "result_n") builder
+    in
+    let result = build_zext result i64 "zext_n" builder in
+    ok result
   | CApplication (func, arg) ->
-    let* callee = codegen_immexpr env func in
-    let* arg = codegen_immexpr env arg in
+    let callee, is_function = codegen_immexpr env func in
+    let* callee = callee in
+    let* arg = fst @@ codegen_immexpr env arg in
     let func_ptr =
       let alloc_closure = lookup_function_exn "peducoml_alloc_closure" the_module in
       let fnty = function_type i64 [| i64; i64 |] in
@@ -121,7 +135,9 @@ let codegen_cexpr env = function
         fnty
         alloc_closure
         [| build_pointercast callee i64 "ptr_to_i64_n" builder
-         ; params callee |> Base.Array.length |> const_int i64
+         ; (if is_function
+            then params callee |> Base.Array.length |> const_int i64
+            else const_int i64 0)
         |]
         "peducoml_alloc_closure_n"
         builder
@@ -129,13 +145,57 @@ let codegen_cexpr env = function
     let apply = lookup_function_exn "peducoml_apply" the_module in
     let fnty = function_type i64 [| i64; i64 |] in
     ok @@ build_call fnty apply [| func_ptr; arg |] "peducoml_apply_n" builder
-  | _ -> failwith "TODO"
+  | CIf (condition, then_branch, else_branch) ->
+    let condition, _ = codegen_immexpr env condition in
+    let* condition = condition in
+    let zero = const_int i64 0 in
+    let condition_value = build_icmp Icmp.Ne condition zero "condition_value_n" builder in
+    let start_bb = insertion_block builder in
+    let the_function = block_parent start_bb in
+    let then_bb = append_block context "then_branch_n" the_function in
+    position_at_end then_bb builder;
+    let then_branch, _ = codegen_immexpr env then_branch in
+    let* then_branch = then_branch in
+    let new_then_bb = insertion_block builder in
+    let else_bb = append_block context "else_branch_n" the_function in
+    position_at_end else_bb builder;
+    let else_branch, _ = codegen_immexpr env else_branch in
+    let* else_branch = else_branch in
+    let new_else_bb = insertion_block builder in
+    let merge_bb = append_block context "if_context_n" the_function in
+    position_at_end merge_bb builder;
+    let incoming = [ then_branch, new_then_bb; else_branch, new_else_bb ] in
+    let phi = build_phi incoming "if_phi_n" builder in
+    Llvm.position_at_end start_bb builder;
+    Llvm.build_cond_br condition_value then_bb else_bb builder |> ignore;
+    Llvm.position_at_end new_then_bb builder;
+    Llvm.build_br merge_bb builder |> ignore;
+    Llvm.position_at_end new_else_bb builder;
+    Llvm.build_br merge_bb builder |> ignore;
+    Llvm.position_at_end merge_bb builder;
+    ok phi
+  | CConstructList (arg_value, arg_list) ->
+    let arg_value, _ = codegen_immexpr env arg_value in
+    let* arg_value = arg_value in
+    let arg_list, _ = codegen_immexpr env arg_list in
+    let* arg_list = arg_list in
+    let add_to_list = lookup_function_exn "peducoml_add_to_list" the_module in
+    let fnty = function_type i64 [| i64; i64 |] in
+    let result =
+      build_call
+        fnty
+        add_to_list
+        [| arg_list; arg_value |]
+        "peducoml_add_to_list_n"
+        builder
+    in
+    ok result
 ;;
 
 let rec codegen_aexpr env = function
   | ACExpr cexpr -> codegen_cexpr env cexpr
   | ALet (id, cexpr, aexpr) ->
-    let alloca = build_alloca i64 (string_of_unique_id id ^ "_n") builder in
+    let alloca = build_alloca i64 (string_of_unique_id id) builder in
     let* cexpr = codegen_cexpr env cexpr in
     build_store cexpr alloca builder |> ignore;
     let env = Base.Map.Poly.set env ~key:id ~data:alloca in
@@ -150,7 +210,7 @@ let codegen_global_scope_function env (func : global_scope_function) =
     (Base.Array.zip_exn (Base.List.to_array arg_list) (params func))
     ~f:(fun (name, value) ->
       match name with
-      | ImmId id -> set_value_name (string_of_unique_id id ^ "_n") value
+      | ImmId id -> set_value_name (string_of_unique_id id) value
       | _ -> failwith "Do arguments need to be immexprs?");
   let basic_block = append_block context "entry" func in
   position_at_end basic_block builder;
@@ -161,7 +221,7 @@ let codegen_global_scope_function env (func : global_scope_function) =
       ~f:(fun (name, value) acc ->
         match name with
         | ImmId id ->
-          let alloca = build_alloca i64 (string_of_unique_id id ^ "_n") builder in
+          let alloca = build_alloca i64 (string_of_unique_id id) builder in
           build_store value alloca builder |> ignore;
           Base.Map.Poly.set acc ~key:id ~data:alloca
         | _ -> failwith "Do they?")
