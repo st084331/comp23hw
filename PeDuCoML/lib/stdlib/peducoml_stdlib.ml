9ea117505c7a70b7ab18cddc90d3a32277165870
Danila-Pechenev <danil-pechenev@mail.ru>
2023-11-22 21:07:53 +0300

    Add comparison functions to stdlib

--- PeDuCoML/lib/stdlib/peducoml_stdlib.ml
+++ PeDuCoML/lib/stdlib/peducoml_stdlib.ml
@@ -15,6 +15,15 @@ let print_list_scheme =
 
 let print_tuple_scheme = Base.Set.empty (module Base.Int), TArr (TVar (-1), TGround Int)
 
+let compare_lists_scheme =
+  ( Base.Set.empty (module Base.Int)
+  , TArr (TList (TVar (-2)), TArr (TList (TVar (-2)), TGround Bool)) )
+;;
+
+let compare_tuples_scheme =
+  Base.Set.empty (module Base.Int), TArr (TVar (-3), TArr (TVar (-3), TGround Bool))
+;;
+
 let stdlib =
   [ "print_int", print_int_scheme
   ; "print_char", print_char_scheme
@@ -22,5 +31,17 @@ let stdlib =
   ; "print_list", print_list_scheme
   ; "print_tuple", print_tuple_scheme
   ; "print_new_line", print_new_line_scheme
+  ; "compare_lists_eq", compare_lists_scheme
+  ; "compare_lists_neq", compare_lists_scheme
+  ; "compare_lists_gt", compare_lists_scheme
+  ; "compare_lists_gte", compare_lists_scheme
+  ; "compare_lists_lt", compare_lists_scheme
+  ; "compare_lists_lte", compare_lists_scheme
+  ; "compare_tuples_eq", compare_tuples_scheme
+  ; "compare_tuples_neq", compare_tuples_scheme
+  ; "compare_tuples_gt", compare_tuples_scheme
+  ; "compare_tuples_gte", compare_tuples_scheme
+  ; "compare_tuples_lt", compare_tuples_scheme
+  ; "compare_tuples_lte", compare_tuples_scheme
   ]
 ;;
