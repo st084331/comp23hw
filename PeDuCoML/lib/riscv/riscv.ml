863549435bbaffc29898d53eee7c37b4533da756
Ilya Dudnikov <dudnikoff98@gmail.com>
2023-12-09 19:49:54 +0300

    Implement application for functions with one argument

--- PeDuCoML/lib/riscv/riscv.ml
+++ PeDuCoML/lib/riscv/riscv.ml
@@ -4,11 +4,46 @@
 
 open Format
 
-(** [const_int v] returns an integer constant with value [v]. *)
-let const_int num = asprintf "%d" num
+type rv_type =
+  | Imm
+  | Register
+  | Offset
+  | Binding
 
-(** [declare_function name body] returns a new function with name [name] and body [body]. *)
-let declare_function name =
+type rv_value =
+  { value : string
+  ; typ : rv_type
+  }
+
+let imm s = { value = s; typ = Imm }
+let register s = { value = s; typ = Register }
+let offset s = { value = s; typ = Offset }
+let binding s = { value = s; typ = Binding }
+
+let get_load_instruction = function
+  | Imm -> "li"
+  | Register -> "mv"
+  | _ -> "ld"
+;;
+
+let global_functions : (string, rv_value) Hashtbl.t = Hashtbl.create 5
+
+let _ =
+  Hashtbl.add global_functions "peducoml_alloc_closure" (binding "peducoml_alloc_closure")
+;;
+
+let _ = Hashtbl.add global_functions "peducoml_apply" (binding "peducoml_apply")
+let _ = Hashtbl.add global_functions "print_int" (binding "print_int")
+
+(** [const_int v] returns an integer constant with value [v]. *)
+let const_int num = asprintf "%d" num |> imm
+
+(** [declare_function name arg_list] returns a new function with name [name] and arguments [arg_list] and locations where its arguments are stored.
+
+    For example, [declare_function "main" ["arg1", "arg2"]] will return a new function with name ["main"] and a list [[("arg1", "a0"), ("arg2", "a1")]],
+    meaning ["arg1"] will be stored in register ["a0"] and ["arg2"] - in register ["a1"]. *)
+let declare_function name arg_list =
+  (* TODO: stack pointer decrement depends on the number of lets inside *)
   printf
     "    .globl %s\n\
     \    .type %s, @function\n\
@@ -19,18 +54,51 @@ let declare_function name =
     name
     name
     name;
-  name
+  let arg_list =
+    Base.List.foldi arg_list ~init:[] ~f:(fun ind acc arg ->
+      let storage_location =
+        if ind < 8
+        then
+          "a" ^ string_of_int ind |> register
+          (* (ind - 8) * 8 + 16 = (ind - 8) * 8 + 2 * 8 = (ind - 6) * 8 *)
+        else string_of_int ((ind - 6) * 8) ^ "(sp)" |> offset
+      in
+      (arg, storage_location) :: acc)
+  in
+  binding name, arg_list
+;;
+
+(** [lookup_function name] returns [Some f] if a function with name [name] exists, and [None] otherwise. *)
+let lookup_function name = Hashtbl.find_opt global_functions name
+
+(** [lookup_function_exn name] returns [f] if a function with name [name] exists, and raises [Not_found] otherwise. *)
+let lookup_function_exn name = Hashtbl.find global_functions name
+
+(** [build_call callee arg_list] stores arguments from [arg_list] and creates a [call callee] instruction. *)
+let build_call callee arg_list =
+  Base.List.iteri arg_list ~f:(fun ind arg ->
+    if ind < 8
+    then printf "    %s a%d,%s\n" (get_load_instruction arg.typ) ind arg.value
+    else printf "    sd %s,%d(sp)\n" arg.value ((ind - 6) * 8));
+  printf "    call %s\n" callee.value;
+  register "a0"
 ;;
 
 let build_ret value =
   printf
-    "    li a0,%s\n    ld ra,8(sp)\n    ld s0,0(sp)\n    addi sp,sp,16\n    ret"
-    value
+    "    %s a0,%s\n    ld ra,8(sp)\n    ld s0,0(sp)\n    addi sp,sp,16\n    ret\n"
+    (get_load_instruction value.typ)
+    value.value
 ;;
 
 (** [build_load value] creates an [ld dest,value] instruction and returns [dest]. *)
 let build_load value =
   (* TODO: need to figure out how to choose a register where to load *)
-  printf "ld t3,%s" value;
-  "t3"
+  printf "    %s t0,%s\n" (get_load_instruction value.typ) value.value;
+  register "t0"
+;;
+
+let build_store value =
+  printf "    sd %s,%d(sp)\n" value 16;
+  offset "16(sp)"
 ;;
--- PeDuCoML/lib/riscv/riscv_comp.ml
+++ PeDuCoML/lib/riscv/riscv_comp.ml
@@ -15,6 +15,7 @@ module Environment = struct
 
   let find = Base.Map.Poly.find
   let empty = Base.Map.Poly.empty
+  let set = Base.Map.Poly.set
 end
 
 let ( let* ) = bind
@@ -30,6 +31,7 @@ let codegen_immexpr env = function
   | ImmInt num -> ok @@ const_int num
   | ImmChar c -> ok @@ const_int (Base.Char.to_int c)
   | ImmBool v -> ok @@ const_int (Base.Bool.to_int v)
+  | ImmId (GlobalScopeId id) -> ok @@ lookup_function_exn id
   | ImmId (AnfId id) ->
     (match find env (AnfId id) with
      | None -> AnfId id |> unbound |> error
@@ -39,24 +41,40 @@ let codegen_immexpr env = function
 
 let codegen_cexpr env = function
   | CImm immexpr -> codegen_immexpr env immexpr
+  | CApplication (callee, arg) ->
+    (* TODO: atm application only works for functions with one argument *)
+    let* callee = codegen_immexpr env callee in
+    let* arg = codegen_immexpr env arg in
+    ok @@ build_call callee [ arg ]
+    (* let func_ptr =
+      build_call (lookup_function_exn "peducoml_alloc_closure") [ callee; const_int 1 ]
+    in
+    ok @@ build_call (lookup_function_exn "peducoml_apply") [ func_ptr; arg ] *)
   | _ -> failwith "TODO"
 ;;
 
-let codegen_aexpr env = function
+let rec codegen_aexpr env = function
   | ACExpr cexpr -> codegen_cexpr env cexpr
-  | _ -> failwith "TODO"
+  | ALet (id, cexpr, aexpr) ->
+    let* cexpr = codegen_cexpr env cexpr in
+    let env = set env ~key:id ~data:cexpr in
+    codegen_aexpr env aexpr
 ;;
 
 let codegen_global_scope_function env (func : global_scope_function) =
-  let function_name, _, body = func in
+  let function_name, arg_list, body = func in
+  let func, args = declare_function function_name arg_list in
+  let env =
+    Base.List.fold_right args ~init:env ~f:(fun (arg, loc) acc ->
+      set acc ~key:arg ~data:loc)
+  in
   let* body = codegen_aexpr env body in
-  let func = declare_function function_name in
   build_ret body;
   ok func
 ;;
 
 let codegen : global_scope_function list -> (unit, riscv_error) Result.t =
-  (* Format.asprintf ".text\n"; *)
+  (* Format.printf "    .option pic\n"; *)
   let rec codegen env = function
     | [] -> ok ()
     | head :: tail ->
