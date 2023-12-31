8b9eef9aa57f173a987c94a47b05c75b3b96dc7d
Ilya Dudnikov <dudnikoff98@gmail.com>
2023-12-31 18:48:06 +0300

    Fixes according to linter report

--- PeDuCoML/lib/stdlib/peducoml_runtime.ml
+++ PeDuCoML/lib/stdlib/peducoml_runtime.ml
@@ -1,3 +1,7 @@
+(** Copyright 2023-2024, Danila Pechenev, Ilya Dudnikov *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
 open Typing
 
 let no_arg_scheme = Base.Set.empty (module Base.Int), TGround Int
--- PeDuCoML/lib/stdlib/peducoml_runtime.mli
+++ PeDuCoML/lib/stdlib/peducoml_runtime.mli
@@ -2,6 +2,4 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-type riscv_error
-
-val codegen : Anf.global_scope_function list -> (unit, riscv_error) Result.t
+val runtime : (Typing.identifier * Typing.scheme) list
