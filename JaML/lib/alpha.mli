92d593f45aa513048ac0e87d7c291f9709d6a59e
Ilya Pankratov <90714492+i-pankrat@users.noreply.github.com>
2024-01-07 18:52:41 +0300

    Remove alpha conversion (#21)
    Signed-off-by: Maxim Drumov <Drumov.M.A@gmail.com>
    Signed-off-by: Ilya Pankratov <i.pankratov.main@gmail.com>
    Co-authored-by: Maxim Drumov <Drumov.M.A@gmail.com>

--- JaML/lib/alpha.mli
+++
@@ -1,8 +0,0 @@
-(** Copyright 2023-2024, Ilya Pankratov, Maxim Drumov *)
-
-(** SPDX-License-Identifier: LGPL-2.1-or-later *)
-
-open Typedtree
-
-(** Alpha conversion of statements *)
-val alpha : tstatements -> tstatements
