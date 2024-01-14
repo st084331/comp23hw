97df9fc75877919e8070b3a1a2468fbb8363c265
st084331 <71183199+st084331@users.noreply.github.com>
2024-01-14 20:50:35 +0300

    anf instead ast

--- ZRusML/lib/llvm_compiler.mli
+++ ZRusML/lib/llvm_compiler.mli
@@ -3,6 +3,6 @@
 (** SPDX-License-Identifier: LGPL-2.1 *)
 
 open Llvm
-open Ast
+open Anf
 
-val compile : prog -> llmodule
+val codegen_program : Anf.abinding list -> (Llvm.llvalue list, string) result
