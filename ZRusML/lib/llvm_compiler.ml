0084afd7ae8f79ff831f0f02260b8f3c9945de81
st084331 <71183199+st084331@users.noreply.github.com>
2024-01-15 02:43:24 +0300

    type fix

--- ZRusML/lib/llvm_compiler.ml
+++ ZRusML/lib/llvm_compiler.ml
@@ -24,12 +24,13 @@ let codegen_imm = function
   | ImmBool b -> ok (const_int bool_type (Base.Bool.to_int b))
   | ImmIdentifier id ->
     (match Hashtbl.find_opt named_values id with
-     | Some v -> ok (build_load v id builder)
+     | Some v -> ok (build_load int_type v id builder)
      | None ->
        (match lookup_function id the_module with
         | Some v ->
           ok
             (build_call
+               (function_type int_type [| int_type; int_type |])
                (Option.get (lookup_function "addNewPAppliClosure" the_module))
                [| build_pointercast v int_type "cast_pointer_to_int" builder
                 ; params v |> Base.Array.length |> const_int int_type
@@ -81,6 +82,7 @@ and codegen_cexpr = function
     let* arg = codegen_imm argument in
     ok
       (build_call
+         (function_type int_type [| int_type; int_type |])
          (Option.get (lookup_function "applyPAppli" the_module))
          [| calee; arg |]
          "PAppliApplication"
