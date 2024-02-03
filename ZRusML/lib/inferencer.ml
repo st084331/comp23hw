472d4aae4219def933789f293266d5aa49fd1623
Rustam Shangareev <rustam.letitbe@gmail.com>
2024-02-03 19:24:52 +0300

    Reformat inferencer.ml

--- ZRusML/lib/inferencer.ml
+++ ZRusML/lib/inferencer.ml
@@ -1,11 +1,11 @@
 (** Copyright 2023-2024, Rustam Shangareev and Danil Yevdokimov *)
 
 (** SPDX-License-Identifier: LGPL-2.1 *)
-module IntSet = Set.Make (Int)
 
 open Base
 open Ast
 open Typing
+module IntSet = Stdlib.Set.Make (Int)
 
 module R : sig
   type 'a t
