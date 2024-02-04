bbf6c4e065a65479c375ab1af65a8119546d0e87
Rustam Shangareev <rustam.letitbe@gmail.com>
2024-02-04 15:26:30 +0300

    Reformat llvm compiler

--- ZRusML/lib/llvm_compiler.ml
+++ ZRusML/lib/llvm_compiler.ml
@@ -6,6 +6,7 @@ open Llvm
 open Result
 open Ast
 open Anf
+open Ast_pretty
 
 let context = global_context ()
 let the_module = create_module context "ZRusML"
@@ -15,40 +16,54 @@ let bool_type = i1_type context
 let named_values : (string, llvalue) Hashtbl.t = Hashtbl.create 50
 let ( let* ) = bind
 
-let codegen_imm = function
-  | ImmInt x -> ok (const_int int_type x)
-  | ImmBool b -> ok (const_int int_type (Base.Bool.to_int b))
-  | ImmIdentifier id ->
-    (match Hashtbl.find_opt named_values id with
-     | Some v -> ok (build_load int_type v id builder)
-     | None ->
-       (match lookup_function id the_module with
-        | Some v ->
-          ok
-            (build_call
-               (function_type int_type [| int_type; int_type |])
-               (Option.get (lookup_function "create_new_apply" the_module))
-               [| build_pointercast v int_type "cast_pointer_to_int" builder
-                ; params v |> Base.Array.length |> const_int int_type
-               |]
-               "PAppliClosure"
-               builder)
-        | None -> error (Format.sprintf "Unknown variable: %s" id)))
+let codegen_imm imm_val =
+  let const_int_type x = ok (const_int int_type x) in
+  let load_build v id = ok (build_load int_type v id builder) in
+  let build_func_call f v =
+    ok
+      (build_call
+         (function_type int_type [| int_type; int_type |])
+         f
+         [| build_pointercast v int_type "cast_pointer_to_int" builder
+          ; params v |> Base.Array.length |> const_int int_type
+         |]
+         "part_applyc"
+         builder)
+  in
+  let find_and_build id =
+    match lookup_function id the_module with
+    | Some v ->
+      build_func_call (Option.get (lookup_function "create_new_apply" the_module)) v
+    | None -> error (Format.sprintf "Unknown variable: %s" id)
+  in
+  let find_and_load id =
+    match Hashtbl.find_opt named_values id with
+    | Some v -> load_build v id
+    | None -> find_and_build id
+  in
+  match imm_val with
+  | ImmInt x -> const_int_type x
+  | ImmBool b -> const_int_type (Base.Bool.to_int b)
+  | ImmIdentifier id -> find_and_load id
 ;;
 
-let codegen_binop = function
-  | Add -> fun x y -> build_add x y "addtmp" builder
-  | Sub -> fun x y -> build_sub x y "subtmp" builder
-  | Mul -> fun x y -> build_mul x y "multmp" builder
-  | Div -> fun x y -> build_sdiv x y "divtmp" builder
-  | Eq -> fun x y -> build_icmp Icmp.Eq x y "eqtmp" builder
-  | Neq -> fun x y -> build_icmp Icmp.Ne x y "neqtmp" builder
-  | Less -> fun x y -> build_icmp Icmp.Slt x y "lesstmp" builder
-  | Leq -> fun x y -> build_icmp Icmp.Sle x y "leqtmp" builder
-  | Gre -> fun x y -> build_icmp Icmp.Sgt x y "gretmp" builder
-  | Geq -> fun x y -> build_icmp Icmp.Sge x y "geqtmp" builder
-  | And -> fun x y -> build_and x y "andtmp" builder
-  | Or -> fun x y -> build_or x y "ortmp" builder
+let codegen_binop binop =
+  let build_op fn_builder nm_builder x y = fn_builder x y nm_builder builder in
+  let get_constructor = function
+    | Add -> build_add
+    | Sub -> build_sub
+    | Mul -> build_mul
+    | Div -> build_sdiv
+    | Eq -> build_icmp Icmp.Eq
+    | Neq -> build_icmp Icmp.Ne
+    | Less -> build_icmp Icmp.Slt
+    | Leq -> build_icmp Icmp.Sle
+    | Gre -> build_icmp Icmp.Sgt
+    | Geq -> build_icmp Icmp.Sge
+    | And -> build_and
+    | Or -> build_or
+  in
+  build_op (get_constructor binop) (show_bin_op binop)
 ;;
 
 let rec codegen_aexpr = function
@@ -63,11 +78,11 @@ let rec codegen_aexpr = function
 and codegen_cexpr = function
   | CUnaryOp (Not, x) ->
     let* x' = codegen_imm x in
-    let res = build_not x' "nottmp" builder in
+    let res = build_not x' "not" builder in
     ok (build_trunc res bool_type "to_bool" builder)
   | CUnaryOp (Minus, x) ->
     let* x' = codegen_imm x in
-    let res = build_neg x' "negtmp" builder in
+    let res = build_neg x' "neg" builder in
     ok (build_zext res int_type "to_int" builder)
   | CBinaryOp (op, l, r) ->
     let* l' = codegen_imm l in
@@ -83,7 +98,7 @@ and codegen_cexpr = function
          (function_type int_type [| int_type; int_type |])
          (Option.get (lookup_function "partially_apply" the_module))
          [| calee; arg |]
-         "PAppliApplication"
+         "papply"
          builder)
   | CIf (cond, then_, else_) ->
     let* cond_val = codegen_imm cond in
@@ -105,48 +120,55 @@ and codegen_cexpr = function
       build_phi [ then_val, new_then_bb; else_val, new_else_bb ] "ifphi" builder
     in
     position_at_end start_bb builder;
-    let (_ : Llvm.llvalue) = build_cond_br cond_val then_bb else_bb builder in
+    let _ = build_cond_br cond_val then_bb else_bb builder in
     position_at_end new_then_bb builder;
-    let (_ : Llvm.llvalue) = build_br merge_bb builder in
+    let _ = build_br merge_bb builder in
     position_at_end new_else_bb builder;
-    let (_ : Llvm.llvalue) = build_br merge_bb builder in
+    let _ = build_br merge_bb builder in
     position_at_end merge_bb builder;
     ok phi
 ;;
 
-let codegen_abind = function
-  | ABind (id, args, body) ->
-    Hashtbl.clear named_values;
-    let ints = Array.make (List.length args) int_type in
-    let ftype = function_type int_type ints in
-    let* func =
-      match lookup_function id the_module with
-      | Some _ -> error "Function already exists"
-      | None -> ok @@ declare_function id ftype the_module
-    in
-    let* names =
-      let rec check acc = function
-        | [] -> ok (List.rev acc)
-        | PImmExpr (ImmIdentifier id) :: xs -> check (id :: acc) xs
-        | PImmWild :: xs -> check ("0_unused" :: acc) xs
-        | _ -> error "Invalid argument"
-      in
-      check [] args
-    in
+let codegen_abind =
+  let rec check_args acc = function
+    | [] -> ok (List.rev acc)
+    | PImmExpr (ImmIdentifier id) :: xs -> check_args (id :: acc) xs
+    | PImmWild :: xs -> check_args ("0unused" :: acc) xs
+    | _ -> error "Invalid argument"
+  in
+  let decl_or_err id ftype the_module =
+    match lookup_function id the_module with
+    | Some _ -> error "Function already exists"
+    | None -> ok @@ declare_function id ftype the_module
+  in
+  let set_names names params =
     Array.iteri
       (fun i a ->
         let name = List.nth names i in
         set_value_name name a)
-      (params func);
-    let bb = append_block context "entry" func in
-    position_at_end bb builder;
+      params
+  in
+  let build_and_updt_values names params =
     Array.iteri
       (fun i a ->
         let name = List.nth names i in
         let alloca = build_alloca int_type name builder in
         let (_ : llvalue) = build_store a alloca builder in
         Hashtbl.add named_values name alloca)
-      (params func);
+      params
+  in
+  function
+  | ABind (id, args, body) ->
+    Hashtbl.clear named_values;
+    let arg_names = check_args [] in
+    let* names = arg_names args in
+    let ints = Array.make (List.length args) int_type in
+    let ftype = function_type int_type ints in
+    let* func = decl_or_err id ftype the_module in
+    set_names names (params func);
+    let bb = append_block context "entry" func in
+    position_at_end bb builder;
+    build_and_updt_values names (params func);
     let* ret_val = codegen_aexpr body in
     let _ =
       if id = "main"
