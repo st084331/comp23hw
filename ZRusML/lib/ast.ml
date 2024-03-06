a8decf67caf5bfef691ddf8f7d274cbe1ccb1984
Данил Евдокимов <st084331@student.spbu.ru>
2024-03-06 20:37:41 +0300

    Update ast.ml

--- ZRusML/lib/ast.ml
+++ ZRusML/lib/ast.ml
@@ -1,4 +1,4 @@
-(** Copyright 2023-2024, Rustam Shangareev and Danil Yevdokimov*)
+(** Copyright 2023-2024, Rustam Shangareev and Danil Yevdokimov *)
 
 (** SPDX-License-Identifier: LGPL-2.1 *)
 
@@ -6,43 +6,43 @@ type id = string [@@deriving show { with_path = false }]
 
 type bin_op =
   | And (**  and *)
-  | Or (**  or  *)
-  | Less (**  <   *)
-  | Leq (**  <=  *)
-  | Gre (**  >   *)
-  | Geq (**  >=  *)
-  | Eq (** =   *)
-  | Neq (**  <>  *)
+  | Or (**  or *)
+  | Less (**  < *)
+  | Leq (**  <= *)
+  | Gre (**  > *)
+  | Geq (**  >= *)
+  | Eq (** = *)
+  | Neq (**  <> *)
   | Add (**  +   *)
   | Sub (**  -   *)
-  | Mul (**  *   *)
-  | Div (**  /  *)
+  | Mul (**  * *)
+  | Div (**  / *)
 
-and un_op =
-  | Not (**  not  *)
+type un_op =
+  | Not (**  not *)
   | Minus (** - *)
 
-and binding = bool * pt * exp
-and case = pt * exp
-and decl = DLet of binding (**  val y = 256   *)
-and prog = decl list
-
-and const =
+type const =
   | CInt of int
   | CBool of bool
 
-and exp =
-  | EConst of const (**    false    *)
-  | EUnOp of un_op * exp (**    not x, -x, !x    *)
-  | EVar of id (**    x    *)
-  | ELet of binding list * exp (**    let x = 256 in 512    *)
-  | EFun of pt * exp (**   fn x -> x   *)
-  | EIf of exp * exp * exp (**    if predicate then x else y    *)
-  | EBinOp of bin_op * exp * exp (**    25 / (7 + -2)    *)
-  | EApp of exp * exp (**    fold a list init    *)
+type pt =
+  | PtWild (**  _ *)
+  | PtVar of id (**  xyz *)
+  | PtConst of const (**  256 *)
 
-and pt =
-  | PtWild (**  _  *)
-  | PtVar of id (**  xyz   *)
-  | PtConst of const (**  256   *)
-[@@deriving show { with_path = false }]
+type binding = bool * pt * exp
+
+and exp =
+  | EConst of const (**    false *)
+  | EUnOp of un_op * exp (**    not x, -x, !x *)
+  | EVar of id (**    x *)
+  | ELet of binding list * exp (**    let x = 256 in 512 *)
+  | EFun of pt * exp (**   fn x -> x *)
+  | EIf of exp * exp * exp (**    if predicate then x else y *)
+  | EBinOp of bin_op * exp * exp (**    25 / (7 + -2) *)
+  | EApp of exp * exp (**    fold a list init *)
+
+type case = pt * exp
+type decl = DLet of binding (**  val y = 256 *)
+type prog = decl list
