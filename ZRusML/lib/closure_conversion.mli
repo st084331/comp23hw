c6b749c684032b8c9a73ca7f669e55af8ca25eb0
Данил Евдокимов <st084331@student.spbu.ru>
2024-03-06 20:37:55 +0300

    Create closure_conversion.mli

---
+++ ZRusML/lib/closure_conversion.mli
@@ -0,0 +1,9 @@
+(** Copyright 2023-2024, Rustam Shangareev and Danil Yevdokimov *)
+
+(** SPDX-License-Identifier: LGPL-2.1 *)
+
+(** Decompose fun to fun args list and fun body *)
+val decompose_fun : Ast.pt list -> Ast.exp -> Ast.pt list * Ast.exp
+
+(** Transform decls to decls without closures *)
+val transform_decls : Ast.decl list -> Ast.decl list
