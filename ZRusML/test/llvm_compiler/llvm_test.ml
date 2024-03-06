e53b6cdba419726287a3b1afccafdf44e9b059b4
Данил Евдокимов <st084331@student.spbu.ru>
2024-03-06 20:39:17 +0300

    Create llvm_test.ml

---
+++ ZRusML/test/llvm_compiler/llvm_test.ml
@@ -0,0 +1,7 @@
+(** Copyright 2023-2024, Rustam Shangareev and Danil Yevdokimov *)
+
+(** SPDX-License-Identifier: LGPL-3.0 *)
+
+open ZRusML_lib.Interpreter
+
+let () = print_prog_result (Stdio.In_channel.input_all Stdlib.stdin)
